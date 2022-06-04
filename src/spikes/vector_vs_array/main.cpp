2dcd1418598814d787fdbda04f6bca461f763041
Olof Bjarnason <olof.bjarnason@gmail.com>
2022-06-04 11:17:48 +0200

    Add stopwatch

--- src/spikes/vector_vs_array/main.cpp
+++ src/spikes/vector_vs_array/main.cpp
@@ -1,5 +1,20 @@
+#include <chrono>
+#include <iostream>
+#include <unistd.h>
 #include "functions.hpp"
 
+using namespace std;
+
 int main() {
+    auto start = chrono::steady_clock::now();
+
+    sleep(1);
+
+    auto end = chrono::steady_clock::now();
+
+    cout << "Elapsed time in microseconds: "
+         << chrono::duration_cast<chrono::microseconds>(end - start).count()
+         << " µs" << endl;
+
     hello();
 }

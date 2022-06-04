d20b2c03c0e9c6242ad985f1fd42eb544a33c903
Olof Bjarnason <olof.bjarnason@gmail.com>
2022-06-04 11:36:02 +0200

    Implement (segfaulting) array test

--- src/spikes/vector_vs_array/main.cpp
+++ src/spikes/vector_vs_array/main.cpp
@@ -2,14 +2,13 @@
 #include <iostream>
 #include "functions.hpp"
 
+#define NUMBER_OF_LOOPS 22
+
 using namespace std;
 
-int main() {
-
-    // std::vector performance
-
+void vector_test() {
     int n = 1;
-    for (int i = 0; i < 25; i++, n *= 2) {
+    for (int i = 0; i < NUMBER_OF_LOOPS; i++, n *= 2) {
         auto start = chrono::steady_clock::now();
         float sum = vector_summer(n);
         auto end = chrono::steady_clock::now();
@@ -23,3 +22,36 @@ int main() {
                 << endl;
     }
 }
+
+void array_test() {
+    int n = 1;
+    for (int i = 0; i < NUMBER_OF_LOOPS; i++, n *= 2) {
+        auto start = chrono::steady_clock::now();
+        float sum = array_summer(n);
+        auto end = chrono::steady_clock::now();
+        long time_us = chrono::duration_cast<chrono::microseconds>(end - start).count();
+        cout
+                << "i=" << i
+                << ",n=" << n
+                << ",kind=std::vector"
+                << ",time=" << time_us << "µs"
+                << ",time/n=" << time_us / (float)n
+                << endl;
+    }
+}
+
+int main() {
+
+    auto start = chrono::steady_clock::now();
+    vector_test();
+    auto end = chrono::steady_clock::now();
+    long time_us = chrono::duration_cast<chrono::microseconds>(end - start).count();
+    cout << "vector total time: " << time_us << "\n\n";
+
+    start = chrono::steady_clock::now();
+    array_test();
+    end = chrono::steady_clock::now();
+    time_us = chrono::duration_cast<chrono::microseconds>(end - start).count();
+    cout << "array total time: " << time_us << "\n\n";
+}
+

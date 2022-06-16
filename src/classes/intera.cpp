7ffa1802bd0e0f16609648ac0f99e6215ea812e9
Julie Gagnon <code.goddess@umop.net>
2022-06-16 05:54:50 -0700

    Pi stack strength.

--- src/classes/intera.cpp
+++ src/classes/intera.cpp
@@ -589,7 +589,7 @@ float InteratomicForce::total_binding(Atom* a, Atom* b)
         // When pi stacking benzene rings, each ring member should only bind 1:1 with a
         // corresponding member of the other ring. The following approximation is an
         // oversimplification for performance's sake.
-        if (a->arom_ring_member && b->arom_ring_member && forces[i]->type == pi)
+        if (forces[i]->type == pi && (a->arom_ring_member || b->arom_ring_member))
         	partial /= 5;
 
         /*if (fabs(partial) >= 10000)

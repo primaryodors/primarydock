de279b50cfc85db9014f658fbb98832443f14861
Julie Gagnon <code.goddess@umop.net>
2022-08-28 19:09:28 -0700

    Activation matrix fix attempt.

--- predict/dock.php
+++ predict/dock.php
@@ -11,58 +11,11 @@
 // Includes
 require("protutils.php");
 require("odorutils.php");
+require("matrices.php");
 
 // Configurable variables
 $dock_retries = 5;
 $max_simultaneous_docks = 4;	// If running this script as a cron, we recommend setting this to half the number of physical cores.
-
-// Set the matrix to immutable constant values derived from the btRho activation.
-$matrix =
-[
-	"TMR1" => [ -1.432411, -2.746519,  2.509232,  2.363341, -3.109768, -3.402768 ],
-	"TMR2" => [  4.639589,  0.024734, -2.108768, -1.090161, -2.695269,  1.679482 ],
-	"TMR3" => [  0.167589, -2.024517,  3.395732,  4.231589,  1.114231, -4.413017 ],
-	"TMR4" => [  1.722089,  0.166982, -1.611769, -2.093911,  1.330481,  2.336232 ],
-	"TMR5" => [ -2.133661,  3.529982,  1.032483, -3.523911,  3.119732,  2.745732 ],
-	"TMR6" => [ -2.557410,  1.492481, -5.381518, -2.909160,  3.353733,  2.692482 ],
-	"TMR7" => [ -0.405411,  0.009982,  2.163982,  3.021839, -3.566268, -1.637518 ],
-];
-
-
-// btRho matrix adjustments
-// Adjust TMR6 to not clash with TMR5.
-$matrix["TMR6"][2] += 3;
-$matrix["TMR6"][5] += 1;
-
-// Equalize Y displacements.
-foreach ($matrix as $region => &$values)
-{
-	$y = ($values[1] + $values[4])/2;
-	$values[1] = $values[4] = $y;
-}
-
-// No increase in distance between TMR5 and TMR3 at the extracellular end.
-$matrix["TMR3"][0] -= 3;
-$matrix["TMR3"][2] -= 1;
-$matrix["TMR5"][0] += 3;
-$matrix["TMR5"][2] -= 1;
-
-// Prevent clash of TMR7 with TMR2 and TMR1. Bring TMR7 closer to TMR3.
-$matrix["TMR7"][3] -= 3;
-$matrix["TMR7"][5] -= 2;
-
-
-# Activation matrix from ADRB2.pepd:
-$matrix =
-[
-	'TMR1' => [  1.928375, -0.231194,  1.514458, -1.745764,  3.297985, -0.673183 ],
-	'TMR2' => [ -1.637074,  2.562085, -0.780735,  6.004886, -0.640827,  1.471912 ],
-	'TMR3' => [  3.991817,  1.599894,  0.494513, -4.396341, -0.118191, -2.558083 ],
-	'TMR4' => [ -3.715700,  4.363474, -0.580147,  4.709310,  0.645814,  0.620629 ],
-	'TMR5' => [  3.459619, -0.976376,  0.259171, -6.087701, -1.455206, -1.813480 ],
-	'TMR6' => [ -8.180727, -1.155262, -2.628376,  3.851160, -5.411479,  0.728764 ],
-	'TMR7' => [  4.153688, -3.598364,  1.721104, -2.335531,  1.117605,  2.223419 ],
-];
 
 
 // Load data
@@ -146,6 +99,8 @@ $fam = family_from_protid($protid);
 switch ($fam)
 {
 	case "TAAR":
+	$matrix = $matrixADRB2;
+	
 	$cenr3_32 = resno_from_bw($protid, "3.32");
 	$cenr5_43 = resno_from_bw($protid, "5.43");
 	$cenr6_48 = resno_from_bw($protid, "6.48");
@@ -156,6 +111,8 @@ switch ($fam)
 	break;
 	
 	default:
+	$matrix = $matrixbtRho;
+	
 	$cenr3_33 = resno_from_bw($protid, "3.33");
 	$cenr3_36 = $cenr3_33 + 3;
 	$cenr5_43 = resno_from_bw($protid, "5.43");
---
+++ predict/statistics.php
@@ -0,0 +1,145 @@
+<?php
+
+function sgn($num)
+{   if ($num < 0) return -1;
+    if ($num > 0) return 1;
+    return 0;
+}
+
+
+
+// http://www.sjsu.edu/faculty/gerstman/StatPrimer/correlation.pdf
+function correlationCoefficient($xarr, $yarr)
+{   if (!is_array($xarr) || !is_array($yarr))   throw new Exception("correlationCoefficient requires two equal length arrays.");
+    if (count($xarr) != count($yarr))           throw new Exception("correlationCoefficient requires two equal length arrays.");
+    if (!count($xarr) || !count($yarr))         throw new Exception("correlationCoefficient passed empty arrays.");
+    
+    $xarr = array_values($xarr);
+    $yarr = array_values($yarr);
+    
+    foreach ($xarr as $i => $x)
+    {   $y = $yarr[$i];
+        if (!is_numeric($x)) $xarr[$i] = (trim($x) == '' ? -500 : 500);
+        if (!is_numeric($y)) $yarr[$i] = (trim($y) == '' ? -500 : 500);
+    }
+    
+    $xavg = array_sum($xarr) / count($xarr); 
+    $yavg = array_sum($yarr) / count($yarr); 
+    
+    $ssxx = 0.0; $ssyy = 0.0; $ssxy = 0.0;
+    
+    foreach ($xarr as $i => $x)
+    {   $y = $yarr[$i];
+        /*if (!is_numeric($x)) $x = (trim($x) == '' ? -500 : 500);
+        if (!is_numeric($y)) $y = (trim($y) == '' ? -500 : 500);*/
+        
+        $ssxx += pow($x - $xavg, 2);
+        $ssyy += pow($y - $yavg, 2);
+        $ssxy += ($x - $xavg)*($y - $yavg);
+    }
+    
+    if ($ssxx == 0 || $ssyy == 0) return '(insufficient data)';
+    
+    $r = $ssxy/pow($ssxx*$ssyy,0.5);
+    return $r;
+}
+
+function array_shuffle($arr)
+{   $arr1 = [];
+    foreach (array_values($arr) as $v) 
+    {   do
+        {   $k = rand(0,16777215);
+        } while (isset($arr1[$k]));
+        $arr1[$k] = $v;
+    }
+    ksort($arr1);
+    return array_values($arr1);
+}
+
+
+function hill($concn, $emax, $ec50, $nH = 2.0)
+{   return $emax / (1.0 + pow($ec50 / $concn, $nH));
+}
+
+function scatter_to_best_fit_hill($arr, $iters = 50)       // Index = concentration; value = observed response.
+{   $emin = min($arr);
+    $emax = max($arr);
+    
+    foreach ($arr as &$a) $a -= $emin;
+    $emax -= $emin;
+    
+    $ec50 = array_sum(array_keys($arr))/count($arr);
+    $nH = 2.0;
+    $delta = 9999999999.99999;
+    
+    if (!$emax) $ec50 = 0;
+    else
+    {
+        $half = $iters/2;
+        for ($i=0; $i<$iters; $i++)
+        {   $delta = 0.0;
+            foreach ($arr as $concn => $obs) $delta += abs($obs - hill(floatval($concn), $emax, $ec50, $nH));
+            
+            $ecold = $ec50;
+            $ec50 *= pow(10, 0.01*rand(-100, 100));
+            
+            $deltan= 0.0;
+            foreach ($arr as $concn => $obs) $deltan+= abs($obs - hill(floatval($concn), $emax, $ec50, $nH));
+            
+            if ($deltan > $delta) $ec50 = $ecold;
+            else $delta = $deltan;
+            
+            if ($i >= $half && $nH >= 0.1 && $nH <= 10)
+            {   $nHold = $nH;
+                $nH *= pow(10, 0.0001*rand(-100, 100));
+                
+                $deltan= 0.0;
+                foreach ($arr as $concn => $obs) $deltan+= abs($obs - hill(floatval($concn), $emax, $ec50, $nH));
+                
+                if ($deltan > $delta) $nH = $nHold;
+                else $delta = $deltan;
+            }
+        }
+    }
+    
+    return [$emax, $emin, $ec50, $nH, $delta];
+}
+
+function median($arr)
+{   if (!is_array($arr)) return $arr;
+    
+    sort($arr);
+    $half = 0.5 * count($arr);
+    
+    if ($half == floor($half))
+    {   return 0.5*$arr[$half] + 0.5*$arr[$half-1];
+    }
+    else
+    {   return $arr[floor($half)];
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

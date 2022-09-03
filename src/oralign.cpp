ef1b7f855805a7e5e26d4e4d2c8e827ebc146977
Julie Gagnon <code.goddess@umop.net>
2022-09-03 14:01:49 -0700

    Create main source file for new feature.

---
+++ src/oralign.cpp
@@ -0,0 +1,12 @@
+#include <cstring>
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include "classes/protein.h"
+
+int main(int argc, char** argv)
+{
+    ;
+}

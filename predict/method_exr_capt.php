327608516dbf2fd21fc71504a1b4123e08d58278
Julie Gagnon <code.goddess@umop.net>
2022-09-05 22:38:54 -0700

    Path dock for TAAR5.

--- predict/method_exr_capt.php
+++ predict/method_exr_capt.php
@@ -1,11 +1,11 @@
 <?php
 
-// method_gpcr_acv.php
+// method_exr_capt.php
 //
 // Performs a dock of an odorant in a receptor using both the inactive and active PDB files.
 //
 // Example call syntax:
-// php -f predict/method_gpcr_acv.php prot=OR1A1 lig=geraniol
+// php -f predict/method_exr_capt.php prot=TAAR5 lig=trimethylamine
 //
 
 // Includes
@@ -88,100 +88,13 @@ if (@$_REQUEST['next'])
 }
 else
 {
-	$protid = @$_REQUEST['prot'] ?: "OR1A1";
+	$protid = @$_REQUEST['prot'] ?: "TAAR5";
 	
-	$ligname = @$_REQUEST['lig'] ?: "geraniol";
+	$ligname = @$_REQUEST['lig'] ?: "trimethylamine";
 }
 
 echo "Beginning dock of $ligname in $protid...\n\n";
 $fam = family_from_protid($protid);
-
-switch ($fam)
-{
-	case "TAAR":
-	$matrix = $matrixADRB2;
-	
-	$cenr3_32 = resno_from_bw($protid, "3.32");
-	$cenr5_43 = resno_from_bw($protid, "5.43");
-	$cenr6_48 = resno_from_bw($protid, "6.48");
-	$cenr6_52 = resno_from_bw($protid, "6.52");
-	$cenr7_43 = resno_from_bw($protid, "7.43");
-	
-	$cenres = "CEN RES $cenr3_32 $cenr5_43 $cenr6_52 $cenr7_43 $cenr6_48";
-	break;
-	
-	default:
-	$matrix = $matrixbtRho;
-	
-	$cenr3_33 = resno_from_bw($protid, "3.33");
-	$cenr3_36 = $cenr3_33 + 3;
-	$cenr5_43 = resno_from_bw($protid, "5.43");
-	$cenr5_47 = $cenr5_43 + 4;
-	$cenr6_40 = resno_from_bw($protid, "6.40");
-	$cenr6_47 = $cenr6_40 + 7;
-	
-	$cenres = "CEN RES $cenr3_33 $cenr3_36 $cenr5_43 $cenr5_47 $cenr6_40 $cenr6_47";
-}
-
-// If the matrix has a global skew, neutralize it.
-$up = [0.0,0.0,0.0];
-$dn = [0.0,0.0,0.0];
-$upq = $dnq = 0;
-foreach ($matrix as $region => $coords)
-{
-	if (substr($region, 0, 3) == "TMR")
-	{
-		$tmrno = intval(substr($region, 3));
-		if ($tmrno & 1)			// Odd numbered TMRs descend, so "up" is N-terminus and "down" is C-terminus.
-		{
-			for ($j=0; $j<2; $j++) $up[$j] += $coords[$j];
-			$upq++;
-			
-			if ($tmrno != 5)
-			{
-				for ($j=0; $j<2; $j++) $dn[$j] += $coords[$j+3];
-				$dnq++;
-			}
-		}
-		else					// Even numbered TMRs go the other way.
-		{
-			for ($j=0; $j<2; $j++) $up[$j] += $coords[$j+3];
-			$upq++;
-			
-			if ($tmrno != 6)
-			{
-				for ($j=0; $j<2; $j++) $dn[$j] += $coords[$j];
-				$dnq++;
-			}
-		}
-	}
-}
-
-if ($upq) for ($j=0; $j<2; $j++) $up[$j] /= $upq;
-if ($dnq) for ($j=0; $j<2; $j++) $dn[$j] /= $dnq;
-
-foreach ($matrix as $region => $coords)
-{
-	if (substr($region, 0, 3) == "TMR")
-	{
-		$tmrno = intval(substr($region, 3));
-		if ($tmrno & 1)
-		{
-			for ($j=0; $j<2; $j++) $matrix[$region][$j  ] -= $up[$j];
-			for ($j=0; $j<2; $j++) $matrix[$region][$j+3] -= $dn[$j];
-		}
-		else
-		{
-			for ($j=0; $j<2; $j++) $matrix[$region][$j  ] -= $dn[$j];
-			for ($j=0; $j<2; $j++) $matrix[$region][$j+3] -= $up[$j];
-		}
-		
-		// Flatten the motion to the XZ plane.
-		//$matrix[$region][1] = $matrix[$region][4] = 0;
-	}
-	
-	foreach ($coords as $i => $c) $matrix[$region][$i] = round($matrix[$region][$i], 3);
-}
 
 
 $tmr2start = $prots[$protid]['region']['TMR2']['start'];
@@ -192,8 +105,6 @@ $exr2start = $tmr4end + 1;
 $exr2end = $tmr5start - 1;
 
 
-$acv_matrix = "";
-foreach ($matrix as $region => $values) $acv_matrix .= "ACVMX $region " . implode(" ", $values)."\n";
 
 $configf = <<<heredoc
 
@@ -201,21 +112,12 @@ PROT pdbs/$fam/$protid.rotated.pdb
 
 LIG sdf/$ligname.sdf
 
-$cenres
-PATH 1 REL 0 0 0
+CEN RES 184 185
+PATH 1 RES 185 191 193
+PATH 2 REL 0 -5 0
+PATH 3 RES 114 165
 
-# NODEPDB 1 pdbs/$fam/$protid.active.pdb
-
-# Activation Matrix.
-# ACVMX region N.x N.y N.z C.x C.y C.z
-$acv_matrix
-
-ACVNODE 1
-
-SIZE 8.0 8.5 8.0
-
-EXCL 1 $cyt1end		# Head, TMR1, and CYT1.
-EXCL $exr2start $exr2end	# EXR2 between TMR4 and TMR5.
+SIZE 7.0 7.5 7.0
 
 POSE 10
 ITER 50
@@ -292,21 +194,10 @@ $average = [];
 
 foreach ($sum as $node => $value)
 {
-	$average[$node?"Active":"Inactive"] = round($value / (@$count[$node] ?: 1), 3);
+	$average[$node] = round($value / (@$count[$node] ?: 1), 3);
 }
 
-if (min($average) > 0 || !isset($average["Active"]) || !isset($average["Inactive"]))
-{
-	$prediction = "Non-Agonist";
-}
-else
-{
-	if ($average["Active"] <= 1.05 * $average["Inactive"])
-		$prediction = "Agonist";
-	else if ($average["Inactive"] <= 1.1 * $average["Active"])
-		$prediction = "Inverse Agonist";
-	else $prediction = "Non-Agonist";
-}
+$prediction = "(insufficient data)";
 
 $actual = best_empirical_pair($protid, $ligname);
 $actual = ($actual > 0) ? "Agonist" : ($actual < 0 ? "Inverse Agonist" : "Non-Agonist");

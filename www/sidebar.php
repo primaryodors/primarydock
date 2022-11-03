92ea894a1c932ac5c4500b52d1e90f06673df850
primaryodors <106125032+primaryodors@users.noreply.github.com>
2022-11-03 07:09:00 -0700

    Allow custom links.

--- www/sidebar.php
+++ www/sidebar.php
@@ -1,4 +1,10 @@
 <?php 
+$sidelinks = [];
+
+$cwd = getcwd();
+chdir(__DIR__);
+if (file_exists('sidelinks.json')) $sidelinks = json_decode(file_get_contents('sidelinks.json'), true);
+chdir($cwd);
 
 ?>
 <ul>
@@ -6,4 +12,13 @@
     <li><a href="receptors.php">Receptors</a></li>
     <li><a href="odorants.php">Odorants</a></li>
     <li><a href="viewer.php" target="_VIEWER">3D Viewer</a></li>
+
+    <?php
+    foreach ($sidelinks as $sl)
+    {
+        echo "<li><a href=\"{$sl['url']}\"";
+        if (@$sl['target']) echo " target=\"{$sl['target']}\"";
+        echo ">{$sl['name']}</a></li>\n";
+    }
+    ?>
 </ul>

a620b395deaf6dac40c5bdd60a8d757e12b014c6
Julie Gagnon <code.goddess@umop.net>
2022-11-06 20:34:23 -0700

    A stupid mistake makes and fixes a stupid mistake.

--- predict/metal.php
+++ predict/metal.php
@@ -20,8 +20,8 @@ foreach ($prots as $k => $v)
 {
     $protid = $k;
     $fam = family_from_protid($protid);
-    if (!@$_REQUEST['force'] && !file_exists("pdbs/$fam/$protid.upright.pdb")) continue;
-    if (file_exists("pdbs/$fam/$protid.metal.pdb")) continue;
+    if (!file_exists("pdbs/$fam/$protid.upright.pdb")) continue;
+    if (!@$_REQUEST['force'] && file_exists("pdbs/$fam/$protid.metal.pdb")) continue;
     
     echo "Processing $protid...\n";
     chdir(__DIR__);

1a5e5ac0486a00116eb2da440f175b8c29f165c7
Julie Gagnon <code.goddess@umop.net>
2023-01-11 20:40:10 -0700

    Spin option on viewer.

--- src/classes/constants.h
+++ src/classes/constants.h
@@ -250,7 +250,7 @@
 #define _hisflip_binding_threshold 25
 
 // Debugging stuff.
-#define _output_each_iter 1
+#define _output_each_iter 0
 #define _dummy_atoms_for_debug 0
 
 #define _DBG_LONELINESS 0

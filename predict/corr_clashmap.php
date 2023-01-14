21aa1f56d395c6128409e7330585140e25bac2b4
Julie Gagnon <code.goddess@umop.net>
2023-01-14 15:51:38 -0700

    Clash map visual.

--- predict/corr_clashmap.php
+++ predict/corr_clashmap.php
@@ -1,9 +1,14 @@
 <?php
 
+chdir(__DIR__);
+require_once("statistics.php");
+require_once("protutils.php");
+
 $inpfile = "dock_results_clashmap.json";
 $rplstrength = 100;
 
-chdir(__DIR__);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 $data = json_decode(file_get_contents($inpfile), true);
 
@@ -34,6 +39,9 @@ foreach ($data as $rcp => $rdat)
 
                 if (!isset($array[$rcp][$tmrno][$bw])) $array[$rcp][$tmrno][$bw] = floatval($mdat) * $valmult;
                 else $array[$rcp][$tmrno][$bw] += floatval($mdat) * $valmult;
+
+                if (!isset($array['all'][$tmrno][$bw])) $array['all'][$tmrno][$bw] = floatval($mdat) * $valmult;
+                else $array['all'][$tmrno][$bw] += floatval($mdat) * $valmult;
             }
         }
 }
@@ -41,4 +49,77 @@ foreach ($data as $rcp => $rdat)
 foreach ($array as $rcp => $rdat) foreach ($rdat as $tmrno => $tdat) ksort($array[$rcp][$tmrno]);
 
 echo "<pre>".print_r($array, true);
+
+
+// Get xyz coordinates for OR1A1 to use as a model.
+
+$orid = "OR1A1";
+$fam = family_from_protid($orid);
+$c = file_get_contents("../pdbs/$fam/$orid.upright.pdb");
+$lines = explode("\n", $c);
+$resxyz = [];
+
+foreach ($lines as $ln)
+{
+    if (substr($ln, 0, 7) == "ATOM   ")
+    {
+        $ln = preg_replace("/\\s+/", " ", $ln);
+        $ln = explode(" ", $ln);
+        if ($ln[2] == "CA")
+        {
+            $resno = intval($ln[4]);
+            $x = floatval($ln[5]);
+            $y = floatval($ln[6]);
+            $z = floatval($ln[7]);
+
+            $resxyz[$orid][$resno] = [$x,$y,$z];
+        }
+    }
+}
+
+
+// Set up an output image.
+
+$pad = 25;
+$w = 360 * 3 + $pad * 2;
+$h = $w / 1.618;
+$im = imagecreatetruecolor($w, $h);
+
+$black = imagecolorallocate($im, 0,0,0);
+$tm1col = imagecolorallocate($im, 64,64,64);
+$tm2col = imagecolorallocate($im, 0,128,255);
+$tm3col = imagecolorallocate($im, 0,255,96);
+$tm4col = imagecolorallocate($im, 160,192,0);
+$tm5col = imagecolorallocate($im, 255,225,0);
+$tm6col = imagecolorallocate($im, 255,128,0);
+$tm7col = imagecolorallocate($im, 225,0,32);
+
+imagefilledrectangle($im, 0,0, $w-1,$h-1, $black);
+
+
+// Each bw number, get the OR1A1 resno, get its xyz coords, plot its azimuth and y value,
+// and draw a filled circle of the TMR color with a size proportional to the perturbation.
+foreach ($array['all'] as $tmrno => $tdat)
+{
+    if ($tmrno < 1 || $tmrno > 7) continue;
+    $ltmcol = "tm{$tmrno}col";
+    $ltmcol = $$ltmcol;
+    foreach ($tdat as $bw => $perturbation)
+    {
+        if ($perturbation <= 0) continue;
+
+        $resno = resno_from_bw($orid, "$tmrno.$bw");
+        $xyz = $resxyz[$orid][$resno];
+        $azimuth = find_angle($xyz[0], -$xyz[2]) + 0.3 - pi();
+        if ($azimuth < 0) $azimuth += pi()*2;
+
+        $x = intval(3 * (180.0 / pi()) * $azimuth + $pad);
+        $y = ($h/2) + $xyz[1]*20 + $pad;
+        $r = sqrt($perturbation)/3;
+
+        imagefilledellipse($im, $x,$y, $r,$r, $ltmcol);
+    }
+}
+
+imagepng($im, "../tmp/clashmap.png");
 

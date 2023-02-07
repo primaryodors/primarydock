73dc3c0f8531d07f92a4d03b5171ead99a28d87b
Julie Gagnon <code.goddess@umop.net>
2023-02-07 07:41:00 -0700

    ...

--- predict/corr_soft_ai.php
+++ predict/corr_soft_ai.php
@@ -3,7 +3,7 @@
 /******************************************************************************/
 /*                                                                            */
 /* To use this PHP script, you must have Composer installed, as well as the   */
-/* php-ai/php-ml library.                                                     */
+/* php-ai/php-ml library:                                                     */
 /*                                                                            */
 /* sudo apt-get install composer                                              */
 /* composer require php-ai/php-ml                                             */
@@ -15,6 +15,16 @@ use Phpml\ModelManager;
 use Phpml\NeuralNetwork\ActivationFunction\PReLU;
 use Phpml\NeuralNetwork\ActivationFunction\Sigmoid;
 
+require_once("odorutils.php");
+
+$training_set   = [];
+$evaluation_set = [];
+$prediction_set = [];
+
+$clsss = ['na',                   // Non-agonist.
+          'a',                    // Agonist.
+          'ia'                    // Inverse agonist.
+         ];
 
 $json_file = "dock_results_soft.json";
 $dock_data = [];
@@ -23,6 +33,45 @@ if (file_exists($json_file))
     $dock_data = json_decode(file_get_contents($json_file), true);
 }
 
-$training_set = [];
-$evaluation_set = [];
-$prediction_set = [];
+$metrics = [];
+foreach ($dock_data as $prot => $ligdat)
+{
+    foreach ($ligdat as $lig => $data)
+    {
+        $odor = find_odorant($lig);
+        $oid = $odor[$oid];
+        foreach ($data as $key => $value)
+        {
+            $metrics[$key] = $key;
+        }
+    }
+}
+
+$aifile = __DIR__."/corr_soft.ai";
+if (file_exists($file)) 
+{
+    $mlp = $modelManager->restoreFromFile($file);
+}
+else
+{ 
+    $mlp = new MLPClassifier(count($metrics), [[100, new PReLU]], $clsss);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

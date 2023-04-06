5213abbebc2bdb66f048a8b9fa94f5ed7433d458
Julie Gagnon <code.goddess@umop.net>
2023-04-05 17:59:01 -0700

    Misc small fixes.

--- src/classes/constants.h
+++ src/classes/constants.h
@@ -306,7 +306,7 @@
 #define _dbg_rock_pic 0
 #define _dbg_hxrax 0
 #define _dbg_mand_conn 0
-#define _dbg_flexion_selection 1
+#define _dbg_flexion_selection 0
 #define _dbg_null_flexions 0
 #define _dbg_repack 0
 

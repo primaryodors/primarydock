a9d03fcb3f622e687eae3ac87071010c96d9062c
Julie Gagnon <code.goddess@umop.net>
2023-04-15 12:04:22 -0700

    Disable soft debugging.

--- src/classes/constants.h
+++ src/classes/constants.h
@@ -305,7 +305,7 @@
 #define _DBG_MOLBB 0
 #define _dbg_bb_rots 0
 #define _dbg_bb_pullaway 0
-#define _dbg_soft 1
+#define _dbg_soft 0
 #define softpocket_mistake 0
 #define _dbg_glomsel 0
 #define _dbg_polsat 0

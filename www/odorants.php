a2d68b8dc01407ff89591da9d858cdbc96312ba3
Julie Gagnon <code.goddess@umop.net>
2023-04-17 19:52:49 -0700

    Odorant list.

--- www/odorants.php
+++ www/odorants.php
@@ -19,56 +19,33 @@ if (!file_exists($pqcorr = "../data/receptor_pq.json"))
     echo " This calculation takes a few moments.<br><br>";
 }
 
-$sortable = [];
-foreach ($odors as $oid => $odor)
-{
-    $fns = $fns1 = strtolower($odor['full_name']); 
-    $fns = str_replace("alpha-",   "", $fns);
-    $fns = str_replace("beta-",    "", $fns);
-    $fns = str_replace("gamma-",   "", $fns);
-    $fns = str_replace("delta-",   "", $fns);
-    $fns = str_replace("epsilon-", "", $fns);
-    $fns = str_replace("omega-",   "", $fns);
-    $fns = preg_replace("/([(][A-Za-z0-9,+-]+[)],?)+-/", "", $fns);
-    $fns = preg_replace("/[(]([0-9]+[rs],?)+[)]-/", "", $fns);
-    $fns = preg_replace("/[A-Za-z](,[A-Za-z])*-/", "", $fns);
-    $fns = preg_replace("/^[a-zA-Z]-/", "", $fns);
-    $fns = preg_replace("/[^a-zA-Z0-9][a-zA-Z]-/", "", $fns);
-    $fns = preg_replace("/([0-9]+,?)+-/", "", $fns);
-    $fns = preg_replace("/[^A-Za-z]/", "", $fns);   
-    
-    $fns1 = preg_replace("/alpha([,-])/",   "&#x3B1;$1", $fns1);
-    $fns1 = preg_replace("/beta([,-])/",    "&#x3B2;$1", $fns1);
-    $fns1 = preg_replace("/gamma([,-])/",   "&#x3B3;$1", $fns1);
-    $fns1 = preg_replace("/delta([,-])/",   "&#x3B4;$1", $fns1);
-    $fns1 = preg_replace("/epsilon([,-])/", "&#x3B5;$1", $fns1);
-    $fns1 = preg_replace("/omega([,-])/",   "&#x3C9;$1", $fns1);
-
-    if (!$fns) $fns = $fns1;
-
-    $sortable[$oid] = $fns . " ~ " . $fns1 . " ~ " . $odor['full_name'];
-}
-
-asort($sortable);
-
-$limit = count($sortable) / 3;
+$limit = count($odors) / 3;
 $i = 0;
 
 echo "<div class=\"odorlist\">\n";
-foreach (array_keys($sortable) as $oid)
+for ($l = 1; $l < 28; $l++)
 {
-    $odor = $odors[$oid];
-    $fnu = urlencode($odor['full_name']);
-    // echo "<!-- {$sortable[$oid]} -->\n";
-    $pettia = explode(" ~ ", $sortable[$oid]);
-    echo "<a href=\"odorant.php?o=$oid\">{$pettia[1]}</a><br>\n";
-
-    $i++;
-    if ($i > $limit)
+    if ($l <= 26) echo "<h2>".chr(64+$l)."</h2>";
+    else echo "<h2>Others</h2>";
+    foreach (array_keys($odors) as $oid)
     {
-        echo "</div>\n";
-        echo "<div class=\"odorlist\">\n";
-        $i = 0;
+        $odor = $odors[$oid];
+        $C = strtoupper(substr(trim_prefixes($odor['full_name']), 0, 1));
+        if ($l <= 26 && $C != chr(64+$l)) continue;
+        if ($l > 26 && ($C >= 'A' && $C <= 'Z')) continue;
+        $fnu = urlencode($odor['full_name']);
+        echo "<a href=\"odorant.php?o=$oid\">".hellenicize($odor['full_name'])."</a> ";
+        // echo trim_prefixes($odor['full_name']);
+        echo "<br>\n";
+
+        /*$i++;
+        if ($i > $limit)
+        {
+            echo "</div>\n";
+            echo "<div class=\"odorlist\">\n";
+            $i = 0;
+        }*/
     }
+    echo "<hr>";
 }
 echo "</div>\n";

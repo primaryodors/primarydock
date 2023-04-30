c17a3da52148691d6fd562959e0e8c34c6ff1ed1
Julie Gagnon <code.goddess@umop.net>
2023-04-30 13:53:47 -0700

    Ensure sdf exists (requires PHP).

--- test/config_test.sh
+++ test/config_test.sh
@@ -1,5 +1,29 @@
 clear
 make code || exit 1
+
+LIG=$(cat $1 | grep 'LIG ')
+for (( i=2; i <= "$#"; i++ )); do
+    if [ "${!i}" = "--lig" ]
+    then
+        i=$((i+1))
+        LIG="${!i}"
+    fi
+done
+LIG="${LIG/LIG /}"
+LIG="${LIG/sdf\//}"
+LIG="${LIG/.sdf/}"
+
+WHPHP=$(which php)
+if [ $WHPHP ]
+then
+    RESULT=$(php -f data/fetch_sdf.php $LIG)
+
+    if echo $RESULT | grep -q 'Odorant not found'; then
+        echo $RESULT
+        exit 1
+    fi
+fi
+
 bin/primarydock "$@"
 
 WHSOX=$(which play)

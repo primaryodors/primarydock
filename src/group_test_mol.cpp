b0cf65ddf4949f8bea242dfc9ddc820ecc2cf6c4
Julie Gagnon <code.goddess@umop.net>
2023-05-22 07:49:22 -0700

    SDF feature on group test.

--- src/group_test_mol.cpp
+++ src/group_test_mol.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <cmath>
 #include <cstring>
+#include <sys/stat.h>
 #include "classes/group.h"
 
 using namespace std;
@@ -12,7 +13,24 @@ int main(int argc, char** argv)
 {
     Molecule m("Testing");
 
-    if (argc > 1) m.from_smiles(argv[1]);
+    if (argc > 1)
+    {
+        struct stat buffer;   
+        if (stat(argv[1], &buffer) == 0)
+        {
+            FILE* fp = fopen(argv[1], "rb");
+
+            fseek(fp, 0L, SEEK_END);
+            int size = ftell(fp);
+            rewind(fp);
+
+            char contents[size+4];
+            fread(contents, size, 1, fp);
+            fclose(fp);
+            m.from_sdf(contents);
+        }
+        else m.from_smiles(argv[1]);
+    }
     else m.from_smiles("CCO");
 
     std::vector<std::shared_ptr<AtomGroup>> mgrp = AtomGroup::get_potential_ligand_groups(&m);

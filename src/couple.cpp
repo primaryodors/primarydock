3ea5b9b5bbe4dc694d99d787b23ceb45690d4428
Julie Gagnon <code.goddess@umop.net>
2023-05-30 21:47:28 -0700

    Couple searches GPCR for motifs.

--- src/couple.cpp
+++ src/couple.cpp
@@ -50,5 +50,36 @@ int main(int argc, char** argv)
     fclose(fp);
     cout << "G-protein: loaded " << gnax.get_seq_length() << " residues." << endl;
 
-    //
+    int bw3_50 = gpcr.get_bw50(3), bw4_50 = gpcr.get_bw50(4), bw7_50 = gpcr.get_bw50(7);
+
+    int c438, c359, c756;
+
+    if (bw3_50 > 0) c359 = bw3_50 + 9;
+    else
+    {
+        int q = gpcr.search_sequence(100, 200, "DRYXAICXPLXY");
+        if (q < 1)
+        {
+            cout << "Cannot find 3.59. Coupling fail." << endl;
+            return -1;
+        }
+        else c359 = q + 10;
+    }
+
+    if (bw4_50 > 0) c438 = bw4_50 - 12;
+    else c438 = c359 + 5;
+
+    if (bw7_50 > 0) c756 = bw7_50 + 6;
+    else
+    {
+        int q = gpcr.search_sequence(250, 400, "PMLNPLIYSLRNKD");
+        if (q < 1)
+        {
+            cout << "Cannot find 7.56. Coupling fail." << endl;
+            return -1;
+        }
+        else c756 = q + 10;
+    }
+
+    cout << "GPCR contact residues: " << c359 << " " << c438 << " " << c756 << endl;
 }

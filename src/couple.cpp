4ef0c7b7c8e650545c06d49e66fdf54661f469f6
Julie Gagnon <code.goddess@umop.net>
2023-06-05 17:56:59 -0700

    Coupling code.

--- src/couple.cpp
+++ src/couple.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <string>
+#include <vector>
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
@@ -11,10 +12,111 @@
 
 using namespace std;
 
-Protein *ggpcr, *ggnax;
+class Contact
+{
+    public:
+    Protein *prot1 = nullptr, *prot2 = nullptr;
+    AminoAcid *aa1 = nullptr, *aa2 = nullptr;
+    std::string cfgstr1, cfgstr2;
+
+    void interpret_cfgs()
+    {
+        // TODO:
+    }
+}
+
+class MovablePiece
+{
+    public:
+    Protein* prot = nullptr;
+    AminoAcid *start_residue = nullptr, *end_residue = nullptr;
+    AminoAcid *first_pivot = nullptr, *last_pivot = nullptr;
+    std::vector<std::string> cfgstrs;
+
+    void interpret_cfgs()
+    {
+        // TODO:
+    }
+
+    function do_motion(SCoord move_amt)
+    {
+        if (!prot) throw 0xbadc0de;
+        if (!start_residue || !end_residue) throw 0xbadc0de;
+        if (!first_pivot && !last_pivot) throw 0xbadc0de;
+
+        // Check the direction of motion and correct if necessary.
+        Point rel = move_amt;
+        if (first_pivot)
+        {
+            Point pivf = first_pivot->get_CA_location();
+            Point old = start_residue->get_CA_location();
+            Point nouion = old.add(rel);
+            nouion = nouion.multiply_3d_distance(pivf, old.get_3d_distance(pivf) / nouion.get_3d_distance(pivf) );
+            rel = nouion.subtract(old);
+        }
+        if (last_pivot)
+        {
+            Point pivf = last_pivot->get_CA_location();
+            Point old = end_residue->get_CA_location();
+            Point nouion = old.add(rel);
+            nouion = nouion.multiply_3d_distance(pivf, old.get_3d_distance(pivf) / nouion.get_3d_distance(pivf) );
+            rel = nouion.subtract(old);
+        }
+
+        // Do the motion.
+        prot->move_piece(start_residue->get_residue_no(), end_residue->get_residue_no(), (SCoord)rel);
+        Point srca = prot->get_residue(start_residue->get_residue_no()-1)->get_CA_location().add(rel);
+        Point eca  = prot->get_residue(end_residue->get_residue_no()+1)->get_CA_location().add(rel);
+
+        // If first pivot, rotate first pivot thru start residue - 1 about first pivot to align with start residue.
+        if (first_pivot)
+        {
+            prot->rotate_piece(first_pivot->get_residue_no(), start_residue->get_residue_no()-1, start_residue->get_residue_no()-1,
+                srca, first_pivot->get_residue_no());
+        }
+
+        // If no first pivot, rotate head through start residue - 1 about last pivot to align with start residue.
+        else
+        {
+            prot->rotate_piece(1, start_residue->get_residue_no()-1, start_residue->get_residue_no()-1,
+                srca, last_pivot->get_residue_no());
+        }
+
+        // If last pivot, rotate end residue + 1 thru last pivot about last pivot to align with end residue.
+        if (last_pivot)
+        {
+            prot->rotate_piece(end_residue->get_residue_no()+1, last_pivot->get_residue_no(), end_residue->get_residue_no()+1,
+                eca, last_pivot->get_residue_no());
+        }
+
+        // If no last pivot, rotate rotate end residue + 1 thru tail about first pivot to align with end residue.
+        else
+        {
+            prot->rotate_piece(end_residue->get_residue_no()+1, 99999, end_residue->get_residue_no()+1,
+                eca, first_pivot->get_residue_no());
+        }
+
+        // Realign segment to new locations of start residue - 1 and end residue + 1.
+
+    }
+
+    function do_rotation(SCoord axis, float theta)
+    {
+        // Check the direction of rotation and correct if necessary.
+
+        // TODO:
+
+    }
+}
+
+Protein *g_prot1, *g_prot2;         // Global protein pointers; not necessarily G-proteins.
 AminoAcid *pivot1, *pivot2, *pivot3, *pivot4, *pivot5, *pivot6, *pivot7;
-Molecule** gcm;
+Molecule** g_contacts_as_mols;
 AminoAcid *sbb = nullptr, *sba = nullptr;
+
+std::string prot1fname, prot2fname;
+std::vector<Contact> contacts;
+std::vector<MovablePiece> segments;
 
 const float montecarlo_theta = fiftyseventh * 1;
 const float montecarlo_xform = 0.5;
@@ -32,12 +134,68 @@ float residue_energy()
     int i;
     float e = 0;
 
-    for (i=0; gcm[i]; i++)
+    for (i=0; g_contacts_as_mols[i]; i++)
     {
-        e += gcm[i]->get_intermol_binding(gcm);
+        e += g_contacts_as_mols[i]->get_intermol_binding(g_contacts_as_mols);
     }
 
     return e;
+}
+
+int interpret_cfg_param(char** words)
+{
+    int i;
+
+    if (!strcmp(words[0], "PROT1"))
+    {
+        prot1fname = words[1];
+        return 2;
+    }
+    else if (!strcmp(words[0], "PROT2"))
+    {
+        prot2fname = words[1];
+        return 2;
+    }
+    else if (!strcmp(words[0], "CONTACT"))
+    {
+        Contact c;
+        c.cfgstr1 = words[1];
+        c.cfgstr2 = words[2];
+        contacts.push_back(c);
+        return 3;
+    }
+    else if (!strcmp(words[0], "SEGMENT"))
+    {
+        MovablePiece p;
+        for (i=1; words[i]; i++)
+            p.cfgstrs.push_back(words[i]);
+
+        return i;
+    }
+
+    return 0;
+}
+
+void read_cfg_lines(FILE* fp)
+{
+    char buffer[1024];
+    char** words;
+    int i;
+    while (!feof(fp))
+    {
+        fgets(buffer, 1015, fp);
+        words = chop_spaced_words(buffer);
+        if (!words) continue;
+        for (i=0; words[i]; i++) if (words[i][0] == '#')
+        {
+            words[i] = nullptr;
+            break;
+        }
+
+        if (words[0]) interpret_cfg_param(words);
+
+        delete words;
+    }
 }
 
 void iteration_callback(int iter)
@@ -50,8 +208,8 @@ void iteration_callback(int iter)
 
     char buffer[8];
     sprintf(buffer, "TMR%d", tmrno);
-    sr = ggpcr->get_region_start(buffer);
-    er = ggpcr->get_region_end(buffer);
+    sr = g_prot1->get_region_start(buffer);
+    er = g_prot1->get_region_end(buffer);
 
     switch (tmrno)
     {
@@ -68,7 +226,7 @@ void iteration_callback(int iter)
     }
 
     float e = residue_energy(), e1 = 0, theta;
-    e -= ggpcr->get_internal_clashes(sr, er, false)*_kJmol_cuA*protein_clash_penalty;
+    e -= g_prot1->get_internal_clashes(sr, er, false)*_kJmol_cuA*protein_clash_penalty;
 
     int i;
 
@@ -77,16 +235,16 @@ void iteration_callback(int iter)
         axis = Point( i==0 ? 1000 : 0, i==1 ? 1000 : 0, i==2 ? 1000 : 0 );
 
         theta = frand(-montecarlo_theta, montecarlo_theta);
-        ggpcr->rotate_piece(sr, er, pivot, axis, theta);
+        g_prot1->rotate_piece(sr, er, pivot, axis, theta);
         e1 = residue_energy();
-        e1 -= ggpcr->get_internal_clashes(sr, er, false)*_kJmol_cuA*protein_clash_penalty;
+        e1 -= g_prot1->get_internal_clashes(sr, er, false)*_kJmol_cuA*protein_clash_penalty;
         if (e1 >= e)
         {
             e = e1;
         }
         else
         {
-            ggpcr->rotate_piece(sr, er, pivot, axis, -theta);
+            g_prot1->rotate_piece(sr, er, pivot, axis, -theta);
         }
 
         // Do not move salt bridge acid.
@@ -104,9 +262,9 @@ void iteration_callback(int iter)
         }
 
         axis.scale(frand(-montecarlo_xform, montecarlo_xform));
-        ggpcr->move_piece(sr, er, (SCoord)axis);
+        g_prot1->move_piece(sr, er, (SCoord)axis);
         e1 = residue_energy();
-        e1 -= ggpcr->get_internal_clashes(sr, er, false)*_kJmol_cuA*protein_clash_penalty;
+        e1 -= g_prot1->get_internal_clashes(sr, er, false)*_kJmol_cuA*protein_clash_penalty;
         if (e1 >= e)
         {
             e = e1;
@@ -114,11 +272,11 @@ void iteration_callback(int iter)
         else
         {
             axis.scale(-axis.magnitude());
-            ggpcr->move_piece(sr, er, (SCoord)axis);
+            g_prot1->move_piece(sr, er, (SCoord)axis);
         }
     }
 
-    float c = ggpcr->get_internal_clashes(sr, er, true, 5);
+    float c = g_prot1->get_internal_clashes(sr, er, true, 5);
 
     // for (i=1; i<tmrno; i++) cout << "     ";
     // cout << c << endl;
@@ -139,7 +297,7 @@ void do_template_homology(const char* template_path)
         tpl.load_pdb(fp);
         fclose(fp);
 
-        ggpcr->homology_conform(&tpl);
+        g_prot1->homology_conform(&tpl);
     }
 }
 
@@ -151,12 +309,12 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    Protein gpcr("GPCR");
-    Protein gnax("GNAX");
+    Protein p1("Prot1");
+    Protein p2("Prot2");
     FILE* fp;
 
-    ggpcr = &gpcr;
-    ggnax = &gnax;
+    g_prot1 = &p1;
+    g_prot2 = &p2;
 
     fp = fopen(argv[1], "rb");
     if (!fp)
@@ -164,312 +322,41 @@ int main(int argc, char** argv)
         cout << "Please ensure " << argv[1] << " exists and is readable." << endl;
         return -1;
     }
-    gpcr.load_pdb(fp);
+    read_cfg_lines(fp);
     fclose(fp);
-    cout << "GPCR: loaded " << gpcr.get_seq_length() << " residues." << endl;
 
-    fp = fopen(argv[2], "rb");
-    if (!fp)
-    {
-        cout << "Please ensure " << argv[2] << " exists and is readable." << endl;
-        return -1;
-    }
-    gnax.load_pdb(fp);
-    fclose(fp);
-    cout << "G-protein: loaded " << gnax.get_seq_length() << " residues." << endl;
+    // TODO: Command line args.
+    
 
-    const char* output_fname;
-    if (argc > 3) output_fname = argv[3];
-    else output_fname = "output/coupled.pdb";
-    cout << "Output filename is " << output_fname << endl;
+    int i, n;
+    p1.load_pdb(prot1fname);
+    p2.load_pdb(prot2fname);
 
-    int bw1_50 = gpcr.get_bw50(1),
-        bw2_50 = gpcr.get_bw50(2),
-        bw3_50 = gpcr.get_bw50(3),
-        bw4_50 = gpcr.get_bw50(4),
-        bw45_50 = gpcr.get_bw50(45),
-        bw5_50 = gpcr.get_bw50(5),
-        bw6_50 = gpcr.get_bw50(6),
-        bw7_50 = gpcr.get_bw50(7);
-
-    int c438, c359, c756;
-
-    if (bw3_50 > 0) c359 = bw3_50 + 9;
-    else
-    {
-        int q = gpcr.search_sequence(100, 200, "DRYXAICXPLXY");
-        if (q < 1)
-        {
-            cout << "Cannot find 3.59. Coupling fail." << endl;
-            return -1;
-        }
-        else c359 = q + 10;
-    }
-
-    if (bw4_50 > 0) c438 = bw4_50 - 12;
-    else c438 = c359 + 6;
-
-    if (bw7_50 > 0) c756 = bw7_50 + 6;
-    else
-    {
-        int q = gpcr.search_sequence(250, 400, "PMLNPLIYSLRNKD");
-        if (q < 1)
-        {
-            cout << "Cannot find 7.56. Coupling fail." << endl;
-            return -1;
-        }
-        else c756 = q + 10;
-    }
-
-    cout << "GPCR contact residues: " << c359 << " " << c438 << " " << c756 << endl;
-
-    int q35 = gnax.search_sequence(1, 1000, "EKQLQKD", 6);
-    int d215 = gnax.search_sequence(120, 1000, "GIFETKFQVD");
-    int e392 = gnax.search_sequence(gnax.get_end_resno()-10, 1000, "YELL");
-
-    if (q35 < 1 || d215 < 1 || e392 < 1)
-    {
-        cout << "Cannot find G-protein motifs. Coupling fail." << endl;                 // My whole adult life has been coupling fails.
-        return -1;
-    }
-
-    q35 += 8;
-    d215 += 9;
-    e392 += 1;
-
-    cout << "G-protein contact residues: " << q35 << " " << d215 << " " << e392 << endl;
-
-
-    // Upright both proteins.
-    cout << "Turning GPCR upright..." << endl;
-    gpcr.upright();
-
-    cout << "Turning G-protein upright..." << endl;
-    Point pt35x4(0,0,0);
-    int i, j=0;
-
-    for (i=q35; i>1 && j<4; i--)
-    {
-        AminoAcid* aa = gnax.get_residue(i);
-        if (aa)
-        {
-            pt35x4 = pt35x4.add(aa->get_CA_location());
-            j++;
-        }
-    }
-
-    if (j)
-    {
-        pt35x4.x /= j;
-        pt35x4.y /= j;
-        pt35x4.z /= j;
-    }
-
-    Point pt1x4(0,0,0);
-
-    j=0;
-    for (i=1; i<q35 && j<4; i++)
-    {
-        AminoAcid* aa = gnax.get_residue(i);
-        if (aa)
-        {
-            pt1x4 = pt1x4.add(aa->get_CA_location());
-            j++;
-        }
-    }
-
-    if (j)
-    {
-        pt1x4.x /= j;
-        pt1x4.y /= j;
-        pt1x4.z /= j;
-    }
-
-    Point axis = pt1x4.subtract(pt35x4);
-    Rotation rot = align_points_3d(axis, Point(-1000,0,0), Point(0,0,0));
-    gnax.rotate_piece(1, 99999, rot, e392);
-
-    axis = gnax.get_residue(e392)->get_CA_location().subtract(pt35x4);
-    axis.x = 0;
-    rot = align_points_3d(axis, Point(0,1000,0), Point(0,0,0));
-    gnax.rotate_piece(1, 99999, rot, e392);
-
-    rot.v = (SCoord)Point(-1000,0,0);
-    rot.a = fiftyseventh*54;
-    gnax.rotate_piece(1, 99999, rot, e392);
-
-    gnax.move_piece(1, 99999, Point(0, -50, 0));
-    gpcr.move_piece(1, 99999, Point(0, 0, 0));
-
-
-
-    // Do a homology with the OR51E2 cryo-EM model.
-    // Yes this is hard coded for now. Have to design a flat file to hold the params.
-    cout << "Applying homology from cryo-EM model..." << endl;
-    do_template_homology();
-
-
-    // Move TMR6 out of the way sooner than later.
-    cout << "Moving TMR6..." << endl;
-
-    AminoAcid* aa;
-
-    for (i=6; i<=11; i++)
-    {
-        aa = gpcr.get_residue(bw6_50+i);
-        if (aa)
-        {
-            char c = aa->get_letter();
-            if (c == 'R' || c == 'K')
-            {
-                AminoAcid* aa1 = gpcr.get_residue(bw45_50 + 1);
-                if (aa1)
-                {
-                    c = aa1->get_letter();
-                    if (c == 'D' || c == 'E')
-                    {
-                        sbb = aa;
-                        sba = aa1;
-                        cout << "Found potential salt bridge between " << *aa << " and " << *aa1 << endl;
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    Point ptdest, ptsrc;
-    Point ptcen(0,0,0);
-
-    pivot6 = nullptr;
-
-    float salt_reach = 0;
-    float salt_angle = M_PI;
-    if (sba && sbb)
-    {
-        salt_angle = find_3d_angle(sbb->get_atom("CB")->get_location(), sba->get_CA_location(), sbb->get_CA_location());
-        salt_reach = sba->get_reach() + sbb->get_reach() * (0.5 + 0.5 * cos(salt_angle)) + 2;
-    }
-
-    if (sba && sbb)
-    {
-        ptsrc = sbb->get_CA_location();
-        float salt_gap = sbb->get_CA_location().get_3d_distance(sba->get_CA_location());
-        float salt_delta = fmax(salt_gap - salt_reach, 0);
-        // cout << "Moving " << *sbb << " " << salt_delta << "A." << endl;
-        ptdest = sba->get_CA_location();
-        ptdest = ptdest.multiply_3d_distance(&ptsrc, salt_delta/salt_gap);
-        // cout << "Acid at " << sba->get_CA_location() << " so moving base from " << ptsrc << " to " << ptdest << endl;
-        gpcr.rotate_piece(gpcr.get_region_start("TMR6"), gpcr.get_region_end("TMR6"), sbb->get_residue_no(), ptdest, bw6_50-2);
-
-        Molecule* tobridge[4];
-        tobridge[0] = (Molecule*)sba;
-        tobridge[1] = (Molecule*)sbb;
-        tobridge[2] = nullptr;
-
-        cout << "Forming salt bridge..." << endl;
-        Molecule::conform_molecules(tobridge, 50);
-
-        pivot6 = sbb;
-    }
-    else
-    {
-        aa = gpcr.get_residue(bw5_50);
-        if (!aa) cout << "No BW5.50 residue." << endl;
-        axis = aa->get_CA_location();
-
-        aa = gpcr.get_residue(bw7_50-5);
-        if (!aa) cout << "No BW7.45 residue." << endl;
-        axis = axis.subtract(aa->get_CA_location());
-
-        rot.v = axis;
-        rot.a = fiftyseventh * 15;
-        gpcr.rotate_piece(gpcr.get_region_start("TMR6"), gpcr.get_region_end("TMR6"), rot, bw6_50-2);
-
-        pivot6 = gpcr.get_residue(bw6_50-2);
-    }
-
-
-    // Line up CYT3 to at least point to TMR6.
-    cout << "Moving CYT3..." << endl;
-    gpcr.rotate_piece(gpcr.get_region_end("TMR5")+1, gpcr.get_region_start("TMR6")-1, gpcr.get_region_start("TMR6")-1,
-        gpcr.get_residue(gpcr.get_region_start("TMR6"))->get_CA_location(), gpcr.get_region_end("TMR5"));
-
-
-    // Superimpose YELL with 7.56.
-    cout << "Aligning C-terminus of G-protein..." << endl;
-
-    ptdest = gpcr.get_residue(c756)->get_CA_location();
-    ptsrc  = gnax.get_residue(e392)->get_CA_location();
-
-    // Move YELL partway towards the midpoint of 5.64 and 6.33.
-    j = 4;
-    if (bw5_50 > 0 && bw6_50 > 0)
-    {
-        ptdest = ptdest.multiply_3d_distance(&ptcen, 4);
-
-        aa = gpcr.get_residue(bw5_50 + 14);
-        if (aa)
-        {
-            ptdest = ptdest.add(aa->get_CA_location());
-            j++;
-        }
-
-        aa = gpcr.get_residue(bw6_50 - 17);
-        if (aa)
-        {
-            ptdest = ptdest.add(aa->get_CA_location());
-            j++;
-        }
-
-        ptdest = ptdest.multiply_3d_distance(&ptcen, 1.0/j);
-    }
-
-    gnax.move_piece(1, 99999, (SCoord)ptdest.subtract(ptsrc));
-
-
-    // Rotate to align Q35 with *4.38.
-    cout << "Aligning rotation of G-protein..." << endl;
-    ptdest = gpcr.get_residue(c438)->get_CA_location();
-    ptsrc  = gnax.get_residue(q35)->get_CA_location();
-    ptcen = gnax.get_residue(e392)->get_CA_location();
-    ptsrc.y = ptdest.y = ptcen.y;
-    rot = align_points_3d(ptsrc, ptdest, ptcen);
-    gnax.rotate_piece(1, 99999, rot, e392);
-
-
-
-    // Do the positional fine tuning.
-    gpcr.set_pdb_chain('A');
-    gnax.set_pdb_chain('B');
-
-    std::vector<AminoAcid*> cr = gpcr.get_contact_residues(&gnax);
-
-    int n = cr.size();
-    cout << "Contact residues: ";
-    for (i=0; i<n; i++) cout << *cr[i] << " ";
-    cout << endl;
-
-    pivot1 = gpcr.get_residue(bw1_50 - 9);
-    pivot2 = gpcr.get_residue(bw2_50 + 7);
-    pivot3 = gpcr.get_residue(bw3_50 - 14);
-    pivot4 = gpcr.get_residue(bw4_50);
-    pivot5 = gpcr.get_residue(bw5_50);
-    pivot7 = gpcr.get_residue(bw7_50 - 8);
-
-    Molecule* contact_mols[n+4];
-
+    n = contacts.size();
     for (i=0; i<n; i++)
     {
-        contact_mols[i] = (Molecule*)cr[i];
+        contacts[i].prot1 = &p1;
+        contacts[i].prot2 = &p2;
+        contacts[i].interpret_cfgs();
     }
-    contact_mols[i] = nullptr;
-    gcm = contact_mols;
 
-    cout << "Fine tuning positions..." << endl;
-    Molecule::conform_molecules(contact_mols, 200, &iteration_callback);
-    cout << endl;
+    n = segments.size();
+    for (i=0; i<n; i++)
+    {
+        segments[i].prot = &p1;
+        segments[i].interpret_cfgs();
+    }
 
+
+    // TODO: p1 homology.
+
+
+    // Now rotate p2 to align as many contacts as possible.
+
+    // Next, move p2 away from p1 (along barycenter-to-barycenter axis) until no clashes.
+
+    // Finally, iteratively wiggle the segments around to get optimal contacts and minimal clashes.
+    // Include small transformations and rotations of p2 to search the conformational space.
 
 
     // Write the output file.

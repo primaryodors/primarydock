18670e361c5895506d1ff7abbfb9d02b5e0f1c2b
Julie Gagnon <code.goddess@umop.net>
2023-06-10 22:01:51 -0700

    Consolidated most of makefile tests into unit test master.

--- test/unit_test_master.sh
+++ test/unit_test_master.sh
@@ -3,6 +3,8 @@ cd "$(dirname "$0")"
 if [ ! -f "received" ]; then mkdir -p "received"; fi
 cd ..
 
+clear
+
 make code > test/received/make_code.txt
 CODE_RESULT=$?
 if [ $CODE_RESULT -eq 0 ]; then
@@ -10,6 +12,72 @@ if [ $CODE_RESULT -eq 0 ]; then
 else
     echo "Make code FAILED."
 fi
+
+
+REPORT="test/point_test.approved.txt"
+./test/point_test >test/point_test.received.txt
+RESULT=$(diff --color --unified $REPORT test/point_test.received.txt)
+if [ -z "$RESULT" ]; then
+    echo "Point test succeeded."
+else
+    echo "Point test FAILED."
+fi
+
+
+REPORT="test/atom_test.approved.txt"
+./test/atom_test >test/atom_test.received.txt
+RESULT=$(diff --color --unified $REPORT test/atom_test.received.txt)
+if [ -z "$RESULT" ]; then
+    echo "Atom test succeeded."
+else
+    echo "Atom test FAILED."
+fi
+
+
+REPORT="test/aniso_test.approved.txt"
+./test/aniso_test >test/aniso_test.received.txt
+RESULT=$(diff --color --unified $REPORT test/aniso_test.received.txt)
+if [ -z "$RESULT" ]; then
+    echo "Anisotropy test succeeded."
+else
+    echo "Anisotropy test FAILED."
+fi
+
+
+# TODO: This is where mol_assem_test would go, but currently its output must be checked visually.
+
+
+REPORT="test/amino_test.approved.txt"
+./test/amino_test >test/amino_test.received.txt
+RESULT=$(diff --color --unified $REPORT test/amino_test.received.txt)
+if [ -z "$RESULT" ]; then
+    echo "Amino test succeeded."
+else
+    echo "Amino test FAILED."
+fi
+
+
+# TODO: A way to check these automatically.
+REPORT="test/protein_test.approved.txt"
+./test/protein_test AAAAAAAAAA > $REPORT
+# Straight Strand PDB.
+echo "Content of test.pdb:" >> $REPORT
+cat test.pdb >> $REPORT
+# Alpha helix PDB.
+echo "Content of test_alpha.pdb:" >> $REPORT
+cat test_alpha.pdb >> $REPORT
+# Beta pleated PDB.
+echo "Content of test_beta.pdb:" >> $REPORT
+cat test_beta.pdb >> $REPORT
+# 3.10 helix PDB.
+echo "Content of test_310.pdb:" >> $REPORT
+cat test_310.pdb >> $REPORT
+# Pi helix PDB.
+echo "Content of test_pi.pdb:" >> $REPORT
+cat test_pi.pdb >> $REPORT
+# SDF of most recent PDB.
+echo "Content of test2.sdf:" >> $REPORT
+sed '2d' test2.sdf >> $REPORT
 
 
 bin/primarydock test/testTAAR8.config --colorless --pose 5 --iter 50 --congress > test/received/TAAR8_CAD.txt
@@ -25,7 +93,7 @@ if [ "$TAAR_RESULT" -eq "0" ]; then
         ASP201=$( cat test/received/TAAR8_CAD.txt | grep -m 1 "Asp201: " )
         ASP201="${ASP201/Asp201: /}"
         ASP201="${ASP201/[.][0-9]*/}"
-        if [[ $ASP111 -gt "-35"  ]] || [[ $ASP201 -gt "-35"  ]]; then
+        if [[ $ASP111 -gt "-30"  ]] || [[ $ASP201 -gt "-30"  ]]; then
             echo "TAAR test FAILED: bad contacts."
         else
             echo "TAAR test succeeded."

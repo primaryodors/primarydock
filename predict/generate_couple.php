747713cfa63eb39cca494d783af07efaf112131d
primaryodors <106125032+primaryodors@users.noreply.github.com>
2023-06-22 08:23:15 -0700

    Formatting.

--- predict/generate_couple.php
+++ predict/generate_couple.php
@@ -12,20 +12,20 @@ $Gprots = [];
 $c = file_get_contents("data/gprots_aligned.txt");
 foreach (explode("\n", $c) as $ln)
 {
-	if (substr($ln, 0, 1) == '#') continue;
-	$gpid = trim(substr($ln, 0, 10));
-	if ($gpid)
-	{
-		if (file_exists("pdbs/Gprot/$gpid.pdb")) $Gprots[] = $gpid;
-	}
+    if (substr($ln, 0, 1) == '#') continue;
+    $gpid = trim(substr($ln, 0, 10));
+    if ($gpid)
+    {
+        if (file_exists("pdbs/Gprot/$gpid.pdb")) $Gprots[] = $gpid;
+    }
 }
 
 print_r($Gprots);
 
 foreach (@$argv as $a)
 {
-	  $a = explode('=',$a,2);
-	  $_REQUEST[$a[0]] = (count($a)>1) ? $a[1] : true;
+    $a = explode('=',$a,2);
+    $_REQUEST[$a[0]] = (count($a)>1) ? $a[1] : true;
 }
 
 $max_simultaneous_couples = 4;
@@ -35,45 +35,45 @@ if (!file_exists($cpl_dir)) mkdir($cpl_dir);
 
 if (@$_REQUEST['next'])
 {
-	  $cmd = "ps -ef | grep ':[0-9][0-9] bin/couple' | grep -v grep";
-	  exec($cmd, $results);
-	  if (!@$_REQUEST['force'] && trim(@$results[$max_simultaneous_couples-1])) die("Already running.\n".print_r($results, 1));
-	  $already = implode("\n", $results);
+    $cmd = "ps -ef | grep ':[0-9][0-9] bin/couple' | grep -v grep";
+    exec($cmd, $results);
+    if (!@$_REQUEST['force'] && trim(@$results[$max_simultaneous_couples-1])) die("Already running.\n".print_r($results, 1));
+    $already = implode("\n", $results);
 
-	  $gpcrid = @$_REQUEST['rcp'] ?: false;
-	  $gpid = @$_REQUEST['gprot'] ?: false;
+    $gpcrid = @$_REQUEST['rcp'] ?: false;
+    $gpid = @$_REQUEST['gprot'] ?: false;
 	  
-	  foreach (array_keys($prots) as $rcpid)
-	  {
+    foreach (array_keys($prots) as $rcpid)
+    {
         if (false!==strpos($already, "/$rcpid"))
-		    {
-			    continue;
-		    }
+        {
+            continue;
+        }
 
-		    if ($gpcrid && $gpcrid != $rcpid && !preg_match("/^$gpcrid$/", $rcpid) ) continue;
-		    foreach ($Gprots as $gprotid)
-		    {
-		        if ($gpid && $gpid != $gprotid) continue;
+        if ($gpcrid && $gpcrid != $rcpid && !preg_match("/^$gpcrid$/", $rcpid) ) continue;
+        foreach ($Gprots as $gprotid)
+        {
+            if ($gpid && $gpid != $gprotid) continue;
 
-		        $fam = family_from_protid($rcpid);
-		        if (!file_exists("$cpl_dir/$fam")) mkdir("$cpl_dir/$fam");
+            $fam = family_from_protid($rcpid);
+            if (!file_exists("$cpl_dir/$fam")) mkdir("$cpl_dir/$fam");
 
-		        $fname = "$cpl_dir/$fam/{$rcpid}_{$gprotid}.pdb";
-		        if (!file_exists($fname))
-		        {
-		            $gpcrid = $rcpid;
-		            $gpid = $gprotid;
-		            goto _found_next;
+            $fname = "$cpl_dir/$fam/{$rcpid}_{$gprotid}.pdb";
+            if (!file_exists($fname))
+            {
+                $gpcrid = $rcpid;
+                $gpid = $gprotid;
+                goto _found_next;
             }
-		    }
-	  }
+        }
+    }
 	  
-	  die("All done!");
+    die("All done!");
 }
 else
 {
     $gpcrid = @$_REQUEST['rcp'] ?: "OR1A1";
-	  $gpid = @$_REQUEST['gprot'] ?: "hGNAL";
+    $gpid = @$_REQUEST['gprot'] ?: "hGNAL";
 }
 	
 _found_next:

5515613c688d1bdfbc7eecf22561a0de50581f91
Julie Gagnon <code.goddess@umop.net>
2023-06-24 10:59:07 -0700

    Finish arraying molecule assembly tests.

--- test/mol_assem_tests.sh
+++ test/mol_assem_tests.sh
@@ -5,11 +5,13 @@
 
 allmols=("benzene" "toluene" "toluene1" "#cyclohexane" "#cyclopentane" "#cyclobutane" "#cyclopropane" "#cyclobutene" "#cyclobutadiene" "#cyclopropene" \
          "#calicene" "#2_6-xylenol" "#proline" "#total_substitution" "#o-methylfuranium_ion" \
-         \
+         "#naphthalene" "#diphenyl_ether" "#azobenzene" "#imidazole" "leaf_alcohol" \
+         "#glucose" "#tetrathioglucose" "#hexathioglucose" \
         )
 allsmiles=("c{C1}1ccccc1!" "C{C1}c1ccccc1!" "c{C1}1ccccc1!C" "C{C1}1CCCCC1!" "C{C1}1CCCC1!" "C{C1}1CCC1!" "C{C1}1CC1!" "C{C1}1=CCC1!" "C{C1}1=CC=C1!" "C{C1}1=CC1!" \
          "C{C1}1(C=CC=C1!)=C2C=C2" "O{O1}c1c(C)cccc1!C" "N{N1}1[C@H](C(=O)O)CCC1!" "C{C1}c1c([Cl])c(CSC)c(O)c(C(=O)[O-])c1![NH3+]" "c{C1}1cc[o+](C)c1!" \
-         \
+         "c{C1}1c2ccccc2ccc1!" "c{C1}1ccccc1!Oc2ccccc2" "c{C1}1ccccc1!N=Nc2ccccc2" "n{N1}1c[nH]cc1!" "C{C1}C/C=C\CCO" \
+         "C{C1}([C@@H]1[C@H]([C@@H]([C@H](C(O1!)O)O)O)O)O" "C{C1}([C@@H]1[C@H]([C@@H]([C@H](C(S1!)S)O)S)S)O" "C{C1}([C@@H]1[C@H]([C@@H]([C@H](C(S1!)S)S)S)S)S" \
         )
 
 RED="\033[38;5;226m\033[48;5;196m"
@@ -38,13 +40,4 @@ for i in ${!allmols[@]}; do
     fi
 done
 
-
-# test/mol_assem_test 'c{C1}1c2ccccc2ccc1!' test/naphthalene.received.sdf
-# test/mol_assem_test 'c{C1}1ccccc1!Oc2ccccc2' test/diphenyl_ether.received.sdf
-# test/mol_assem_test 'c{C1}1ccccc1!N=Nc2ccccc2' test/azobenzene.received.sdf
-# test/mol_assem_test 'n{N1}1c[nH]cc1!' test/imidazole.received.sdf
-# test/mol_assem_test 'C{C1}C/C=C\CCO' test/leaf_alcohol.received.sdf
-# test/mol_assem_test 'C{C1}([C@@H]1[C@H]([C@@H]([C@H](C(O1!)O)O)O)O)O' test/glucose.received.sdf
-# test/mol_assem_test 'C{C1}([C@@H]1[C@H]([C@@H]([C@H](C(S1!)S)O)S)S)O' test/tetrathioglucose.received.sdf
-# test/mol_assem_test 'C{C1}([C@@H]1[C@H]([C@@H]([C@H](C(S1!)S)S)S)S)S' test/hexathioglucose.received.sdf
 

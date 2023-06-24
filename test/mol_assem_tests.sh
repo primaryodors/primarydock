f68d5d42ee360973d61b8916dbee2651987760cc
Julie Gagnon <code.goddess@umop.net>
2023-06-24 14:12:34 -0700

    Express unit test.

--- test/mol_assem_tests.sh
+++ test/mol_assem_tests.sh
@@ -7,11 +7,13 @@ allmols=("benzene" "toluene" "toluene1" "#cyclohexane" "#cyclopentane" "#cyclobu
          "#calicene" "#2_6-xylenol" "#proline" "#total_substitution" "#o-methylfuranium_ion" \
          "#naphthalene" "#diphenyl_ether" "#azobenzene" "#imidazole" "leaf_alcohol" \
          "#glucose" "#tetrathioglucose" "#hexathioglucose" \
+         "#mushroom_alcohol" \
         )
 allsmiles=("c{C1}1ccccc1!" "C{C1}c1ccccc1!" "c{C1}1ccccc1!C" "C{C1}1CCCCC1!" "C{C1}1CCCC1!" "C{C1}1CCC1!" "C{C1}1CC1!" "C{C1}1=CCC1!" "C{C1}1=CC=C1!" "C{C1}1=CC1!" \
          "C{C1}1(C=CC=C1!)=C2C=C2" "O{O1}c1c(C)cccc1!C" "N{N1}1[C@H](C(=O)O)CCC1!" "C{C1}c1c([Cl])c(CSC)c(O)c(C(=O)[O-])c1![NH3+]" "c{C1}1cc[o+](C)c1!" \
          "c{C1}1c2ccccc2ccc1!" "c{C1}1ccccc1!Oc2ccccc2" "c{C1}1ccccc1!N=Nc2ccccc2" "n{N1}1c[nH]cc1!" "C{C1}C/C=C\CCO" \
          "C{C1}([C@@H]1[C@H]([C@@H]([C@H](C(O1!)O)O)O)O)O" "C{C1}([C@@H]1[C@H]([C@@H]([C@H](C(S1!)S)O)S)S)O" "C{C1}([C@@H]1[C@H]([C@@H]([C@H](C(S1!)S)S)S)S)S" \
+         "C=CC(O)CCCCC" \
         )
 
 RED="\033[38;5;226m\033[48;5;196m"
--- test/unit_test_master.sh
+++ test/unit_test_master.sh
@@ -10,61 +10,10 @@ cd ..
 
 clear
 
-make code | sed '/^#/d' > test/received/make_code.txt
-CODE_RESULT=$?
-if [ $CODE_RESULT -eq 0 ]; then
-    printf "${GRN}Make code succeeded.${NC}\n"
-else
-    printf "${RED}Make code FAILED.${NC}\n"
-    echo $CODE_RESULT
-fi
 
-
-REPORT="test/point_test.approved.txt"
-./test/point_test | sed '/^#/d' >test/received/point_test.received.txt
-RESULT=$(diff --unified $REPORT test/received/point_test.received.txt)
-if [ -z "$RESULT" ]; then
-    printf "${GRN}Point test succeeded.${NC}\n"
-else
-    printf "${RED}Point test FAILED.${NC}\n"
-    diff --color --unified $REPORT test/received/point_test.received.txt
-fi
-
-
-REPORT="test/atom_test.approved.txt"
-./test/atom_test H | sed '/^#/d' >test/received/atom_test.received.txt
-RESULT=$(diff --unified $REPORT test/received/atom_test.received.txt)
-if [ -z "$RESULT" ]; then
-    printf "${GRN}Atom test succeeded.${NC}\n"
-else
-    printf "${RED}Atom test FAILED.${NC}\n"
-    diff --color --unified $REPORT test/received/atom_test.received.txt
-fi
-
-
-REPORT="test/aniso_test.approved.txt"
-./test/aniso_test --asciiart | sed '/^#/d' >test/received/aniso_test.received.txt
-RESULT=$(diff --unified $REPORT test/received/aniso_test.received.txt)
-if [ -z "$RESULT" ]; then
-    printf "${GRN}Anisotropy test succeeded.${NC}\n"
-else
-    printf "${RED}Anisotropy test FAILED.${NC}\n"
-    diff --color --unified $REPORT test/received/aniso_test.received.txt
-fi
-
+./test/unit_test_express.sh
 
 ./test/mol_assem_tests.sh
-
-
-REPORT="test/amino_test.approved.txt"
-./test/amino_test | sed '/^#/d' >test/received/amino_test.received.txt
-RESULT=$(diff --unified $REPORT test/received/amino_test.received.txt)
-if [ -z "$RESULT" ]; then
-    printf "${GRN}Amino test succeeded.${NC}\n"
-else
-    printf "${RED}Amino test FAILED.${NC}\n"
-    diff --color --unified $REPORT test/received/amino_test.received.txt
-fi
 
 
 REPORT="test/protein_test.approved.txt"

10f11e6ebf623688432a456b2bb6af2fb276fff6
PrimaryOdors.org <julie@primaryodors.org>
2023-08-08 07:05:13 -0700

    Script for internal contacts comparison.

---
+++ predict/icactive.php
@@ -0,0 +1,58 @@
+<?php
+
+$rcpid = @$argv[1] or die("Usage: php -f predict/icactive.php receptor_id\n");
+
+echo "$rcpid\n";
+
+if (substr($rcpid, 0, 2) == "OR")
+{
+    $active_pdbfn = "pdbs/Gprot/hOR51E2-hGNAS2.pdb";
+    $active_chain = "R";
+
+    $inactive_pdbfn = "pdbs/OR51/OR51E2.upright.pdb";
+    $inactive_chain = "A";
+}
+else if (substr($rcpid, 0, 4) == "TAAR")
+{
+    $active_pdbfn = "pdbs/Gprot/mTAAR9-hGNAS2-pea.pdb";
+    $active_chain = "R";
+
+    $inactive_pdbfn = "pdbs/mTAAR9.pdb";
+    $inactive_chain = "A";
+}
+else die("Unsupported protein.\n");
+
+if (!file_exists($active_pdbfn) || !file_exists($inactive_pdbfn)) passthru("bin/pepteditor predict.gprot.pepd");
+
+$active_pairs_raw = [];
+exec("bin/ic $active_pdbfn $active_chain", $active_pairs_raw);
+
+$inactive_pairs_raw = [];
+exec("bin/ic $inactive_pdbfn $inactive_chain", $inactive_pairs_raw);
+
+$active_pairs = [];
+$inactive_pairs = [];
+
+foreach ($active_pairs_raw as $pair)
+{
+    $pieces = explode(" ", $pair);
+    $pairid = str_replace(':', '', $pieces[0]);
+    $energy = floatval($pieces[1]);
+    $radius = floatval(preg_replace("/[^0-9.]/", "", $pieces[2]));
+
+    $active_pairs[$pairid] = [$energy, $radius];
+}
+
+foreach ($inactive_pairs_raw as $pair)
+{
+    $pieces = explode(" ", $pair);
+    $pairid = str_replace(':', '', $pieces[0]);
+    $energy = floatval($pieces[1]);
+    $radius = floatval(preg_replace("/[^0-9.]/", "", $pieces[2]));
+
+    $inactive_pairs[$pairid] = [$energy, $radius];
+}
+
+// TODO:
+// Energy improvements with decrease in radius means contacts made during activation.
+// Energy changes without a significant decrease in radius means repacking.

c3d8263d3a6d1d4df315531c00554cc54f4b03cf
PrimaryOdors.org <julie@primaryodors.org>
2023-08-12 12:02:09 -0700

    TAAR test attempt.

--- src/classes/group.cpp
+++ src/classes/group.cpp
@@ -472,6 +472,8 @@ std::vector<std::shared_ptr<AtomGroup>> AtomGroup::get_potential_ligand_groups(M
             Atom* b = mol->get_atom(j);
             if (!b) continue;
 
+            if (g->get_ionic() && !b->get_charge() && !b->is_polar() && !b->is_pi()) continue;
+
             float r = fmax(0, g->get_center().get_3d_distance(b->get_location()) - 1.5);
             if (r > 2.5 && !a->shares_bonded_with(b))
             {
@@ -489,7 +491,6 @@ std::vector<std::shared_ptr<AtomGroup>> AtomGroup::get_potential_ligand_groups(M
 
             // In general, aliphatics and nonpolar aromatics can be regarded as high similarity, since they tend to
             // be mutually highly soluble in one another. But for grouping of ligand atoms, it is important to
-
             if (simil >= 0.5)
             {
                 if (aliphatic < 3 || b->get_Z() == 1)

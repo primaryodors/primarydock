2d26758a9faece34fea413985f25e92744943451
primaryodors <106125032+primaryodors@users.noreply.github.com>
2023-08-16 13:00:57 -0700

    Clarity.

--- www/odorant.php
+++ www/odorant.php
@@ -198,8 +198,8 @@ window.setTimeout( function()
     <th>log10 ec<sub>50</sub></th>
     <th>Adj. Top</th>
     <th>Antagonist?</th>
-    <th>Hypothesized PQ</th>
-    <th>Associated Perceptual Qualities</th>
+    <th style="margin-right: 5px;">Hypothesized PQ</th>
+    <th>Associated Perceptual Qualities (PQs)</th>
 </tr>
 
 <?php

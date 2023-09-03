96aa914d682c446326126b745eb36d055b4dac62
PrimaryOdors.org <julie@primaryodors.org>
2023-09-03 16:17:26 -0700

    Test is working for default values.

--- src/classes/moiety.cpp
+++ src/classes/moiety.cpp
@@ -13,15 +13,16 @@ int Moiety::contained_by(Molecule* mol, Atom** out_matches)
         Atom* a = mol->get_atom(i);
         if (!a) break;
 
-        if (j = does_atom_match(a, out_matches ? (out_matches + l) : nullptr))
+        if (j = does_atom_match(a, out_matches + l))
         {
             #if _dbg_moieties
             cout << "Matched " << j << " atoms." << endl;
             #endif
             l += j;
-            num_matched += j;
+            num_matched++;
         }
     }
+    out_matches[l] = nullptr;
 
     return num_matched;
 }
@@ -57,15 +58,16 @@ int Moiety::does_atom_match(Atom* a, Atom** out_matches)
     Atom* cursor[20];
     Atom* numbered[10];
 
-    Atom* dirty_atom[molsize];
     int atoms_used = 0;
 
     for (i=0; i<10; i++) numbered[i] = nullptr;
-    for (i=0; i<molsize; i++) dirty_atom[i] = nullptr;
 
     cursor[parens] = a;
+    c = p[0];
+    buffer[0] = c;
+    buffer[1] = 0;
     if (!atom_matches_string(cursor[parens], buffer)) return 0;
-    dirty_atom[atoms_used++] = cursor[parens];
+    out_matches[atoms_used++] = cursor[parens];
 
     for (i=1; i<n; i++)
     {
@@ -92,13 +94,13 @@ int Moiety::does_atom_match(Atom* a, Atom** out_matches)
         Bond** b = cursor[parens]->get_bonds();
         int bn = cursor[parens]->get_geometry();
 
-        bool found = false;
+        bool found = 0;
         for (j=0; j<bn; j++)
         {
             if (!b[j]) continue;
             if (!b[j]->btom) continue;
-            bool used = false;
-            for (l=0; l<atoms_used; l++) if (dirty_atom[l] == b[j]->btom) used = true;
+            bool used = 0;
+            for (l=0; l<atoms_used; l++) if (out_matches[l] == b[j]->btom) used = true;
             if (atom_matches_string(b[j]->btom, buffer))
             {
                 if (card)
@@ -106,16 +108,17 @@ int Moiety::does_atom_match(Atom* a, Atom** out_matches)
                     Bond* between = cursor[parens]->get_bond_between(b[j]->btom);
                     int lcard = between->cardinality;
                     if (between->cardinality > 1 && between->cardinality < 2 && card >= 1 && card <= 2) lcard = card;
-                    if (lcard != card) return false;
+                    if (lcard != card) return 0;
                 }
                 found = true;
                 cursor[parens] = b[j]->btom;
-                dirty_atom[atoms_used++] = b[j]->btom;
+                out_matches[atoms_used++] = b[j]->btom;
                 break;
             }
         }
-        if (!found) return false;
+        if (!found) return 0;
     }
 
-    return true;
+    out_matches[atoms_used] = nullptr;
+    return atoms_used;
 }

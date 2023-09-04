7249130c8d5daae54642a332233843e56127617f
PrimaryOdors.org <julie@primaryodors.org>
2023-09-04 01:54:17 -0700

    Improved moiety recognition.

--- src/classes/moiety.cpp
+++ src/classes/moiety.cpp
@@ -65,124 +65,142 @@ int Moiety::does_atom_match(Atom* a, Atom** out_matches)
     if (!molsize) throw 0xffff;
 
     const char* p = pattern.c_str();
-    int i, j, l, n = strlen(p), result = 0, parens = 0, card = 0;
+    int i, j, l, n = strlen(p), result = 0, parens = 0, card = 0, iter;
     char c, buffer[10];
     Atom* cursor[20];
     Atom* numbered[10];
-
+    Atom* dud_atoms[32];
+    int num_duds = 0;
     int atoms_used = 0;
 
-    for (i=0; i<10; i++) numbered[i] = nullptr;
-
-    cursor[parens] = a;
-    c = p[0];
-    buffer[0] = c;
-    buffer[1] = 0;
-    if (!atom_matches_string(cursor[parens], buffer)) return 0;
-    out_matches[atoms_used++] = cursor[parens];
-
-    for (i=1; i<n; i++)
+    for (iter=0; iter<10; iter++)
     {
-        c = p[i];
+        // cout << endl << iter << endl;
+        atoms_used = parens = card = 0;
+        for (i=0; i<10; i++) numbered[i] = nullptr;
 
-        if (c <= ' ') continue;
-        if (c == '-')
+        cursor[parens] = a;
+        c = p[0];
+        buffer[0] = c;
+        buffer[1] = 0;
+        if (!atom_matches_string(cursor[parens], buffer)) return 0;
+        out_matches[atoms_used++] = cursor[parens];
+
+        for (i=1; i<n; i++)
         {
-            card = 1;
-            continue;
-        }
-        else if (c == '=')
-        {
-            card = 2;
-            continue;
-        }
-        else if (c == '#')
-        {
-            card = 3;
-            continue;
-        }
-        else if (c >= '0' && c <= '9')
-        {
-            if (numbered[c-'0'])
+            c = p[i];
+
+            if (c <= ' ') continue;
+            if (c == '-')
             {
-                Bond* b = cursor[parens]->get_bond_between(numbered[c-'0']);
-                if (!b) return 0;
-                if (card)
-                {
-                    int lcard = b->cardinality;
-                    if (b->cardinality > 1 && b->cardinality < 2 && card >= 1 && card <= 2) lcard = card;
-                    if (lcard != card) return 0;
-                    card = 0;
-                }
-
-                numbered[c-'0'] = nullptr;
+                card = 1;
                 continue;
+            }
+            else if (c == '=')
+            {
+                card = 2;
+                continue;
+            }
+            else if (c == '#')
+            {
+                card = 3;
+                continue;
+            }
+            else if (c >= '0' && c <= '9')
+            {
+                if (numbered[c-'0'])
+                {
+                    Bond* b = cursor[parens]->get_bond_between(numbered[c-'0']);
+                    if (!b) return 0;
+                    if (card)
+                    {
+                        int lcard = b->cardinality;
+                        if (b->cardinality > 1 && b->cardinality < 2 && card >= 1 && card <= 2) lcard = card;
+                        if (lcard != card) return 0;
+                        card = 0;
+                    }
+
+                    numbered[c-'0'] = nullptr;
+                    continue;
+                }
+                else
+                {
+                    numbered[c-'0'] = cursor[parens];
+                    continue;
+                }
+            }
+            else if (c == '(')
+            {
+                parens++;
+                cursor[parens] = cursor[parens-1];
+                continue;
+            }
+            else if (c == ')')
+            {
+                parens--;
+                continue;
+            }
+            else if (c == '[')
+            {
+                const char* bracket = strchr(&p[i], ']');
+                if (!bracket) return 0;
+                j = bracket - p - i;
+                for (l=1; l<j; l++)
+                {
+                    buffer[l-1] = p[i+l];
+                }
+                buffer[l-1] = 0;
+                i += j;
             }
             else
             {
-                numbered[c-'0'] = cursor[parens];
-                continue;
+                buffer[0] = c;
+                buffer[1] = 0;
             }
-        }
-        else if (c == '(')
-        {
-            parens++;
-            cursor[parens] = cursor[parens-1];
-            continue;
-        }
-        else if (c == ')')
-        {
-            parens--;
-            continue;
-        }
-        else if (c == '[')
-        {
-            const char* bracket = strchr(&p[i], ']');
-            if (!bracket) return 0;
-            j = bracket - p - i;
-            for (l=1; l<j; l++)
-            {
-                buffer[l-1] = p[i+l];
-            }
-            buffer[l-1] = 0;
-            i += j;
-        }
-        else
-        {
-            buffer[0] = c;
-            buffer[1] = 0;
-        }
 
-        Bond** b = cursor[parens]->get_bonds();
-        int bn = cursor[parens]->get_geometry();
+            Bond** b = cursor[parens]->get_bonds();
+            int bn = cursor[parens]->get_geometry();
 
-        bool found = 0;
-        for (j=0; j<bn; j++)
-        {
-            if (!b[j]) continue;
-            if (!b[j]->btom) continue;
-            bool used = false;
-            for (l=0; l<atoms_used; l++) if (out_matches[l] == b[j]->btom) used = true;
-            if (used) continue;
-            if (atom_matches_string(b[j]->btom, buffer))
+            bool found = 0;
+            for (j=0; j<bn; j++)
             {
-                if (card)
+                if (!b[j]) continue;
+                if (!b[j]->btom) continue;
+                bool used = false;
+                for (l=0; l<atoms_used; l++) if (out_matches[l] == b[j]->btom) used = true;
+                for (l=0; l<num_duds; l++) if (dud_atoms[l] == b[j]->btom) used = true;
+                if (used) continue;
+                // cout << "Trying " << *cursor[parens] << "~" << *(b[j]->btom) << " for " << buffer << "..." << endl;
+                if (atom_matches_string(b[j]->btom, buffer))
                 {
-                    Bond* between = cursor[parens]->get_bond_between(b[j]->btom);
-                    int lcard = between->cardinality;
-                    if (between->cardinality > 1 && between->cardinality < 2 && card >= 1 && card <= 2) lcard = card;
-                    if (lcard != card) return 0;
-                    card = 0;
+                    if (card)
+                    {
+                        Bond* between = cursor[parens]->get_bond_between(b[j]->btom);
+                        int lcard = between->cardinality;
+                        if (between->cardinality > 1 && between->cardinality < 2 && card >= 1 && card <= 2) lcard = card;
+                        if (lcard != card) return 0;
+                        card = 0;
+                    }
+                    found = true;
+                    // cout << "Good!" << endl;
+                    cursor[parens] = b[j]->btom;
+                    out_matches[atoms_used++] = b[j]->btom;
+                    break;
                 }
-                found = true;
-                cursor[parens] = b[j]->btom;
-                out_matches[atoms_used++] = b[j]->btom;
-                break;
+            }
+            if (!found)
+            {
+                if (cursor[parens] == a) return 0;
+                // cout << *cursor[parens] << " is a dud." << endl;
+                dud_atoms[num_duds++] = cursor[parens];
+                dud_atoms[num_duds] = nullptr;
+                atoms_used = 0;
             }
         }
-        if (!found) return 0;
+        next_iter:
+        if (atoms_used) break;
     }
-
     out_matches[atoms_used] = nullptr;
+    // cout << "Result: " << atoms_used << endl;
     return atoms_used;
 }

e50a5355c627efd8d4b628c21bb8988bcd3962f7
PrimaryOdors.org <julie@primaryodors.org>
2023-09-03 23:32:42 -0700

    Atom group tests have never been ready for prime time.

--- test/unit_test_express.sh
+++ test/unit_test_express.sh
@@ -206,65 +206,8 @@ else
 fi
 
 
-MOLECULE="phenethyl_alcohol"
-test/group_test_mol "sdf/$MOLECULE.sdf" | sed '/^#/d' > testdata/received/groups.$MOLECULE.received.txt
-RESULT=$(diff --unified testdata/groups.$MOLECULE.approved.txt testdata/received/groups.$MOLECULE.received.txt)
-if [ -z "$RESULT" ]; then
-    printf "${GRN}Molecule group test succeeded for $MOLECULE.${NC}\n"
-else
-    printf "${RED}Molecule group test FAILED for $MOLECULE.${NC}\n"
-    diff --color --unified testdata/groups.$MOLECULE.approved.txt testdata/received/groups.$MOLECULE.received.txt
-fi
+# group_tests_mol.sh
 
-MOLECULE="indole"
-test/group_test_mol "sdf/$MOLECULE.sdf" | sed '/^#/d' > testdata/received/groups.$MOLECULE.received.txt
-RESULT=$(diff --unified testdata/groups.$MOLECULE.approved.txt testdata/received/groups.$MOLECULE.received.txt)
-if [ -z "$RESULT" ]; then
-    printf "${GRN}Molecule group test succeeded for $MOLECULE.${NC}\n"
-else
-    printf "${RED}Molecule group test FAILED for $MOLECULE.${NC}\n"
-    diff --color --unified testdata/groups.$MOLECULE.approved.txt testdata/received/groups.$MOLECULE.received.txt
-fi
-
-MOLECULE="pyrazine"
-test/group_test_mol "sdf/$MOLECULE.sdf" | sed '/^#/d' > testdata/received/groups.$MOLECULE.received.txt
-RESULT=$(diff --unified testdata/groups.$MOLECULE.approved.txt testdata/received/groups.$MOLECULE.received.txt)
-if [ -z "$RESULT" ]; then
-    printf "${GRN}Molecule group test succeeded for $MOLECULE.${NC}\n"
-else
-    printf "${RED}Molecule group test FAILED for $MOLECULE.${NC}\n"
-    diff --color --unified testdata/groups.$MOLECULE.approved.txt testdata/received/groups.$MOLECULE.received.txt
-fi
-
-MOLECULE="cinnamaldehyde"
-test/group_test_mol "sdf/$MOLECULE.sdf" | sed '/^#/d' > testdata/received/groups.$MOLECULE.received.txt
-RESULT=$(diff --unified testdata/groups.$MOLECULE.approved.txt testdata/received/groups.$MOLECULE.received.txt)
-if [ -z "$RESULT" ]; then
-    printf "${GRN}Molecule group test succeeded for $MOLECULE.${NC}\n"
-else
-    printf "${RED}Molecule group test FAILED for $MOLECULE.${NC}\n"
-    diff --color --unified testdata/groups.$MOLECULE.approved.txt testdata/received/groups.$MOLECULE.received.txt
-fi
-
-MOLECULE="arabinose"
-test/group_test_mol "sdf/l-arabinose.sdf" | sed '/^#/d' > testdata/received/groups.$MOLECULE.received.txt
-RESULT=$(diff --unified testdata/groups.$MOLECULE.approved.txt testdata/received/groups.$MOLECULE.received.txt)
-if [ -z "$RESULT" ]; then
-    printf "${GRN}Molecule group test succeeded for $MOLECULE.${NC}\n"
-else
-    printf "${RED}Molecule group test FAILED for $MOLECULE.${NC}\n"
-    diff --color --unified testdata/groups.$MOLECULE.approved.txt testdata/received/groups.$MOLECULE.received.txt
-fi
-
-MOLECULE="adenosine"
-test/group_test_mol "sdf/$MOLECULE.sdf" | sed '/^#/d' > testdata/received/groups.$MOLECULE.received.txt
-RESULT=$(diff --unified testdata/groups.$MOLECULE.approved.txt testdata/received/groups.$MOLECULE.received.txt)
-if [ -z "$RESULT" ]; then
-    printf "${GRN}Molecule group test succeeded for $MOLECULE.${NC}\n"
-else
-    printf "${RED}Molecule group test FAILED for $MOLECULE.${NC}\n"
-    diff --color --unified testdata/groups.$MOLECULE.approved.txt testdata/received/groups.$MOLECULE.received.txt
-fi
 
 test/group_test_res | sed '/^#/d' > testdata/received/group_test_res.received.txt
 RESULT=$(diff --unified testdata/group_test_res.approved.txt testdata/received/group_test_res.received.txt)

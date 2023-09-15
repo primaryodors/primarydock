5a7ee2a76979f029d8b3fadabb64d2850a88d452
PrimaryOdors.org <julie@primaryodors.org>
2023-09-14 18:45:07 -0700

    Prediction test.

--- test/unit_test_master.sh
+++ test/unit_test_master.sh
@@ -49,6 +49,20 @@ else
 fi
 
 
+REPORT="testdata/OR51E2_propionate_pred.approved.txt"
+php -f predict/method_icactive.php prot=OR51E2 lig=propionic_acid | tee >( grep '[[]Predicted[]] => ' > testdata/received/OR51E2_propionate_pred.received.txt)
+RESULT=$(diff --unified $REPORT testdata/received/OR51E2_propionate_pred.received.txt)
+if [ -z "$RESULT" ]; then
+    printf "${GRN}OR51E2 propionate prediction test succeeded.${NC}\n"
+else
+    printf "${RED}OR51E2 propionate prediction test FAILED.${NC}\n"
+    diff --color --unified $REPORT testdata/received/OR51E2_propionate_pred.received.txt
+fi
+
+
+# php -f predict/method_icactive.php prot=OR1A1 lig=d-limonene | tee >( grep '[[]Predicted[]] => ' > testdata/received/OR1A1_d-limonene_pred.received.txt)
+
+
 bin/primarydock testdata/test_TAAR8.config --colorless --iter 50 --congress > testdata/received/TAAR8_CAD.txt
 TAAR_RESULT=$?
 if [ "$TAAR_RESULT" -eq "0" ]; then

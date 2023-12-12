628775fe0760885a263f3fc05a5d90568d5e290c
PrimaryOdors.org <julie@primaryodors.org>
2023-12-11 17:53:11 -0700

    Text colors.

--- test/unit_test_master.sh
+++ test/unit_test_master.sh
@@ -49,7 +49,7 @@ else
 fi
 
 
-echo "${CYN}Running prediction tests and docking tests; these will take some time. Please wait.${NC}"
+printf "${CYN}Running prediction tests and docking tests; these will take some time. Please wait.${NC}\n"
 
 REPORT="testdata/OR51E2_propionate_pred.approved.txt"
 # php -f predict/method_icactive.php prot=OR51E2 lig=propionic_acid | tee >( grep '[[]Predicted[]] => ' > testdata/received/OR51E2_propionate_pred.received.txt)

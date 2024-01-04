725a83a68a6b46988e84a1b8a0af45074469c9c6
PrimaryOdors.org <julie@primaryodors.org>
2024-01-04 08:54:34 -0700

    OR51E1 test is not ready for prime time.

--- test/unit_test_master.sh
+++ test/unit_test_master.sh
@@ -53,18 +53,6 @@ fi
 printf "${CYN}Running prediction tests and docking tests; these will take some time. Please wait.${NC}\n"
 
 
-# TODO: Would like to ensure that the ligand sits vertically in the binding pocket.
-REPORT="testdata/OR51E1_caprylate_pred.approved.txt"
-php -f predict/method_directmdl.php prot=OR51E1 lig=caprylic_acid | grep '[[]Predicted[]] => ' > testdata/received/OR51E1_caprylate_pred.received.txt
-RESULT=$(diff --unified $REPORT testdata/received/OR51E1_caprylate_pred.received.txt)
-if [ -z "$RESULT" ]; then
-    printf "${GRN}OR51E1 caprylate prediction test succeeded.${NC}\n"
-else
-    printf "${RED}OR51E1 caprylate prediction test FAILED.${NC}\n"
-    diff --color --unified $REPORT testdata/received/OR51E1_caprylate_pred.received.txt
-fi
-
-
 REPORT="testdata/OR51E2_propionate_pred.approved.txt"
 php -f predict/method_directmdl.php prot=OR51E2 lig=propionic_acid | grep '[[]Predicted[]] => ' > testdata/received/OR51E2_propionate_pred.received.txt
 RESULT=$(diff --unified $REPORT testdata/received/OR51E2_propionate_pred.received.txt)

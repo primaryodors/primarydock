49bc1699f997f7cbb639198b8813548f135daf27
PrimaryOdors.org <julie@primaryodors.org>
2024-01-06 15:21:14 -0700

    Allow 'next' in run_prediction.

--- run_prediction.sh
+++ run_prediction.sh
@@ -8,16 +8,20 @@ if [ -z "$PROT" ]; then
     PROT="OR51E2"
 fi
 
-if [ -z "$LIG" ]; then
+if [[ "$LIG" -eq "next" ]]; then
+    LIG="next=1"
+elif [ -z "$LIG" ]; then
     LIG="propionic_acid"
+else
+    LIG="lig=$LIG"
 fi
 
 FIRST4=${PROT:0:4}
 
 if [[ "$FIRST4" = "OR51" ]]; then
-    php -f predict/method_directmdl.php "prot=$PROT" "lig=$LIG"
+    php -f predict/method_directmdl.php "prot=$PROT" "$LIG"
 elif [[ "$FIRST4" = "OR52" ]]; then
-    php -f predict/method_directmdl.php "prot=$PROT" "lig=$LIG"
+    php -f predict/method_directmdl.php "prot=$PROT" "$LIG"
 else
-    php -f predict/method_fygactive.php "prot=$PROT" "lig=$LIG"
+    php -f predict/method_fygactive.php "prot=$PROT" "$LIG"
 fi

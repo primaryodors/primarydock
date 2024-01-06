348639a0d63f809630b80abc459b9c91606dc235
PrimaryOdors.org <julie@primaryodors.org>
2024-01-05 19:37:21 -0700

    Filename.

--- aroma_predict.sh
+++ aroma_predict.sh
@@ -6,6 +6,6 @@ while read ln; do
   PROT=${ln%%[[:space:]]*}
   if [[ "$PROT" =~ ^(OR[0-9]{1,2}[A-Z]{1,2}[0-9]{1,2}|TAAR[0-9]) ]]; then
     echo "$PROT"
-    bash ./run_prediction "$PROT" "$LIG"
+    bash ./run_prediction.sh "$PROT" "$LIG"
   fi
 done <data/sequences_aligned.txt

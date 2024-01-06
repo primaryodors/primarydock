a5c1848ada29f5934d32a364d342ff84fd26a696
PrimaryOdors.org <julie@primaryodors.org>
2024-01-05 19:36:45 -0700

    Script for aroma profile prediction.

---
+++ aroma_predict.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+LIG="$1"
+
+while read ln; do
+  PROT=${ln%%[[:space:]]*}
+  if [[ "$PROT" =~ ^(OR[0-9]{1,2}[A-Z]{1,2}[0-9]{1,2}|TAAR[0-9]) ]]; then
+    echo "$PROT"
+    bash ./run_prediction "$PROT" "$LIG"
+  fi
+done <data/sequences_aligned.txt

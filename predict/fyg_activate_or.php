fe1124e00af13bdefad32aad30964d1260e56ac3
PrimaryOdors.org <julie@primaryodors.org>
2024-01-11 00:25:59 -0700

    Allow specifying protein ID in fyg_activate php.

--- predict/fyg_activate_or.php
+++ predict/fyg_activate_or.php
@@ -9,9 +9,10 @@ chdir(__DIR__);
 chdir("..");
 foreach ($prots as $protid => $prot)
 {
+    if (isset($argv[1]) && $protid != $argv[1] && !preg_match("/^{$argv[1]}$/", $protid)) continue;
     $fam = family_from_protid($protid);
     $pdbfname_active = "pdbs/$fam/$protid.active.pdb";
-    if (!file_exists($pdbfname_active) || filemtime($pdbfname_active) < filemtime("bin/fyg_activate_or"))
+    if (!file_exists($pdbfname_active) || filemtime($pdbfname_active) < filemtime("bin/fyg_activate_or") || $protid == @$argv[1])
     {
         if (check_already_fyg_activating($protid)) continue;
         build_template();

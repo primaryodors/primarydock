bb2c2945ddba251fa9212523f3f23dac75667532
primaryodors <106125032+primaryodors@users.noreply.github.com>
2024-01-12 15:28:22 -0700

    No FYG-motif activations for direct model receptors.

--- predict/fyg_activate_or.php
+++ predict/fyg_activate_or.php
@@ -11,6 +11,17 @@ foreach ($prots as $protid => $prot)
 {
     if (isset($argv[1]) && $protid != $argv[1] && !preg_match("/^{$argv[1]}$/", $protid)) continue;
     $fam = family_from_protid($protid);
+
+    if (substr($fam, 0, 2) == "OR")
+    {
+        $fno = intval(substr($fam, 2));
+        if ($fno == 51 || $fno == 52)
+        {
+            if (@$argv[1] == $protid) die("OR51 and OR52 receptors should use the direct model method.\n");
+            else continue;
+        }
+    }
+
     $pdbfname_active = "pdbs/$fam/$protid.active.pdb";
     if (!file_exists($pdbfname_active) || filemtime($pdbfname_active) < filemtime("bin/fyg_activate_or") || $protid == @$argv[1])
     {

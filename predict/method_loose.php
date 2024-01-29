a88aaaf3f7a499ca2270f87859e49a8c6c62b5ed
PrimaryOdors.org <julie@primaryodors.org>
2024-01-28 21:06:30 -0700

    Loose method.

--- predict/method_loose.php
+++ predict/method_loose.php
@@ -1,11 +1,13 @@
 <?php
 
-// method_directmdl.php
+// method_loose.php
 //
-// Performs a dock of an odorant inside apo and bound PDB files from cryo-EM or other acquisition methods.
+// Performs a dock of an odorant inside an inactive model, then isolates the ligand binding residues,
+// allowing them to move freely, before constructing an active model that approximates the residues'
+// freely conformed positions.
 //
 // Example call syntax:
-// php -f predict/method_directmdl.php prot=OR51E2 lig=propionic_acid
+// php -f predict/method_loose.php prot=OR1A1 lig=geraniol
 //
 
 chdir(__DIR__);
@@ -76,66 +78,8 @@ function make_prediction($data)
     return $data;
 }
 
-
 chdir(__DIR__);
 chdir("..");
 
-$pdbfname_inactive = str_replace(".upright.pdb", ".apo.pdb", $pdbfname);
-$pdbfname_active = str_replace(".upright.pdb", ".bound.pdb", $pdbfname);
-$paramfname = str_replace(".upright.pdb", ".params", $pdbfname);
-
-if (!file_exists($pdbfname_inactive) && file_exists($pdbfname_active))
-    $pdbfname_inactive = $pdbfname;
-
-if (!file_exists($pdbfname_active) && (substr($protid, 0, 4) == "OR51" || substr($protid, 0, 4) == "OR52"))
-{
-    exec("php -f predict/cryoem_motions.php");
-    exec("bin/pepteditor data/OR51.pepd");
-    exec("bin/pepteditor data/OR52.pepd");
-    exec("bin/pepteditor data/OR5K.pepd");
-}
-
-if (!file_exists($pdbfname_active)) die("No bound model.\n");
-
-$flex_constraints = "";
-if (file_exists($paramfname)) $flex_constraints = file_get_contents($paramfname);
-
-$fam = family_from_protid($protid);
-$pdbfname = $pdbfname_inactive;
-$outfname = "output/$fam/$protid/$protid.$ligname.inactive.dock";
-$cenres = substr($cenres_inactive, 8);
-
-prepare_receptor($pdbfname, "$flxr $iflxr");
-
-// Convert ligand as well.
-prepare_ligand($ligname);
-
-chdir(__DIR__);
-chdir("..");
-if (!file_exists("output/$fam")) mkdir("output/$fam");
-if (!file_exists("output/$fam/$protid")) mkdir("output/$fam/$protid");
-
-if (!@$_REQUEST["acvonly"]) process_dock("i");
-
-
-$pdbfname = $pdbfname_active;
-$outfname = "output/$fam/$protid/$protid.$ligname.active.dock";
-$cenres = substr($cenres_active, 8);
-
-prepare_receptor($pdbfname, "$flxr $aflxr");
-
-$poses = process_dock("a");
-
-// TODO: Separate dynamic_clash_compensation() into FYG-activation and direct-model editions.
-/* if ((!$poses || $best_energy >= 0) && count($clashcomp) && $num_std_devs)
-{
-    dynamic_clash_compensation();
-
-    $pdbfname = $tmpoutpdb;
-    $outfname = "output/$fam/$protid/$protid.$ligname.dynamic.dock";
-    prepare_receptor($pdbfname, "$flxr $aflxr");
-    $poses = process_dock("ad");
-
-    // Delete the tmp PDB.
-    unlink($tmpoutpdb);
-} */
+$pdbfname_inactive = $pdbfname;
+$pdbfname_active = "tmp/$protid.$ligname.loose.pdb";

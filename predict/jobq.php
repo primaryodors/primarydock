3fb13cb03b3282a05a132cce18cbbe84b9d2f9e9
primaryodors <106125032+primaryodors@users.noreply.github.com>
2024-09-18 06:52:41 -0700

    Job queue fixes. (#462)

--- predict/jobq.php
+++ predict/jobq.php
@@ -4,9 +4,15 @@ $procs = [];
 exec("ps -ef | grep obabel | grep -v grep", $procs);
 if (count($procs)) die("Waiting for obabel.");
 
+$dbg = false;
+foreach ($argv as $a) if ($a == 'dbg') $dbg = true;
+
 $max_concurrent = 2;
 $queue = [];
 
+chdir(__DIR__);
+chdir("..");
+require_once("data/odorutils.php");
 chdir(__DIR__);
 chdir("..");
 $dock_results = json_decode(file_get_contents("predict/dock_results.json"), true);
@@ -32,6 +38,27 @@ $procs = [];
 exec("ps -ef | grep \" bin/primarydock \" | grep -v \"sh -c \" | grep -v grep", $procs);
 if (count($procs) >= $max_concurrent) die("Maximum processes.\n");
 
+function uptodate($prot, $lig)
+{
+    global $dock_results, $version;
+    return isset($dock_results[$prot][$lig])
+        && intval($dock_results[$prot][$lig]['version']) >= $version
+        && isset($dock_results[$prot][$lig]['Predicted']);
+}
+
+function running($prot, $lig)
+{
+    global $procs, $dbg;
+    
+    foreach ($procs as $p)
+    {
+        if ($dbg) echo "$p\n$prot | $lig\n\n";
+        if (false!==strpos($p, ".{$prot}_") && false!==strpos($p, ".{$lig}.")) return true;
+    }
+
+    return false;
+}
+
 // Example:
 // primary+ 1342875 1342799 94 18:09 pts/0    01:24:43 bin/primarydock tmp/prediction.OR51E2_i.pelargonic_acid.config
 foreach ($queue as $q)
@@ -40,22 +67,28 @@ foreach ($queue as $q)
     $prot = $q[0];
     $lig = $q[1];
 
-    if (isset($dock_results[$prot][$lig]))
+    if ($lig == "*")
     {
-        if (intval($dock_results[$prot][$lig]['version']) >= $version) continue;
+        $pairs = all_empirical_pairs_for_receptor($prot, true, false);
+        foreach (array_keys($pairs) as $oid)
+        {
+            $lig = $odors[$oid]['full_name'];
+            if (!uptodate($prot, $lig)) if (!running($prot, $lig)) break;
+        }
     }
 
-    foreach ($procs as $p)
-    {
-        if (false===strpos($p, ".{$prot}_")) continue;
-        if (false===strpos($p, ".{$lig}.")) continue;
-        $already = true;
-        break;
-    }
+    $lig = @find_odorant($lig)["full_name"];
+    if (!$lig) continue;
+    $lig = trim(str_replace(" ", "_", $lig));
+
+    if (uptodate($prot, $lig)) continue;
+
+    $already = running($prot, $lig);
+    if ($dbg) echo "already? $already\n";
 
     if (!$already)
     {
-        passthru("./run_prediction.sh $prot '$lig' &");
+        if (!$dbg) passthru("./run_prediction.sh $prot '$lig' &");
         exit;
     }
 }

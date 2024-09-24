0875dbfa3c69d6b70cc5df93620f93990cb51b55
primaryodors <106125032+primaryodors@users.noreply.github.com>
2024-09-24 14:02:18 -0700

    Verbiage.

--- predict/methods_common.php
+++ predict/methods_common.php
@@ -821,7 +821,7 @@ heredoc;
         }
 
         if (!$retvar && $num_poses && !file_exists("tmp/nodelete")) unlink($cnfname);
-        else echo "WARNING: Not deleting a temporary config file in tmp/ because you have selected the debug \"nodelete\" behavior.\n"
+        else echo "WARNING: Not deleting a temporary config file in tmp/.\n"
             ."You should periodically check the tmp/ folder and manually clean out old files that are no longer necessary to keep.\n";
 
         break;

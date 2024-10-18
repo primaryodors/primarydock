e9364ed814ff2fb7666255c364687a6bc50c7416
PrimaryOdors.org <julie@primaryodors.org>
2024-10-17 20:46:34 -0700

    OR2AG2 prediction is better without dynu.

--- predict/methods_common.php
+++ predict/methods_common.php
@@ -889,7 +889,7 @@ heredoc;
                 case "OR1": case "OR2": case "OR3": case "OR4": case "OR5": case "OR6": case "OR7": case "OR8": case "OR9":
                 case "OR10": case "OR11": case "OR12": case "OR13": case "OR14": case "OR15":        // Humans don't have OR15s, but some mammals do.
                 $softness = "1.0";
-                $dynu = "DYNU 5.58 5.32";
+                $dynu = ""; // "DYNU 5.58 5.32";
                 break;
 
                 case "OR51": case "OR52": case "OR56":
